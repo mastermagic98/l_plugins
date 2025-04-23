60afe6e978fdf7ed1e263ece0234eb596d7adeea
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-04-23 19:12:07 +0300

    Update logo_title.js

--- logo_title.js
+++ logo_title.js
@@ -1,61 +1,65 @@
 !function(){
     "use strict";
 
+    // Додаємо переклади
     Lampa.Lang.add({
-        logo_glav_label: {
+        logoplugin_name: {
             en: 'Logos instead of names',
             uk: 'Логотипи замість назв',
             ru: 'Логотипы вместо названий'
         },
-        logo_glav_descr: {
+        logoplugin_description: {
             en: 'Displays movie logos instead of text',
             uk: 'Відображає логотипи фільмів замість тексту',
             ru: 'Отображает логотипы фильмов вместо текста'
         },
-        logo_glav_hide: {
+        logoplugin_hide: {
             en: 'Hide',
-            uk: 'Сховати',
+            uk: 'Приховати',
             ru: 'Скрыть'
         },
-        logo_glav_show: {
+        logoplugin_show: {
             en: 'Show',
-            uk: 'Показати',
-            ru: 'Показать'
+            uk: 'Відображати',
+            ru: 'Отображать'
         }
     });
 
+    // Додаємо параметр у налаштування
     Lampa.SettingsApi.addParam({
         component: "interface",
         param: {
             name: "logo_glav",
             type: "select",
             values: {
-                1: Lampa.Lang.translate('logo_glav_hide'),
-                0: Lampa.Lang.translate('logo_glav_show')
+                1: Lampa.Lang.translate('logoplugin_hide'),
+                0: Lampa.Lang.translate('logoplugin_show')
             },
             default: "0"
         },
         field: {
-            name: Lampa.Lang.translate('logo_glav_label'),
-            description: Lampa.Lang.translate('logo_glav_descr')
+            name: Lampa.Lang.translate('logoplugin_name'),
+            description: Lampa.Lang.translate('logoplugin_description')
         }
     });
 
+    // Основна логіка плагіну
     if (!window.logoplugin) {
         window.logoplugin = !0;
-
-        Lampa.Listener.follow("full", function(a){
-            if (a.type === "complite" && Lampa.Storage.get("logo_glav") !== "1") {
-                var movie = a.data.movie;
-                var url = Lampa.TMDB.api((movie.name ? "tv/" : "movie/") + movie.id + "/images?api_key=" + Lampa.TMDB.key() + "&language=" + Lampa.Storage.get("language"));
-                
-                $.get(url, function(response){
-                    if (response.logos && response.logos.length > 0) {
-                        var path = response.logos[0].file_path;
-                        if (path && path !== "") {
-                            var img = Lampa.TMDB.image("/t/p/original" + path); // SVG підтримка
-                            var html = '<img style="margin-top: 5px; max-height: 200px; max-width: 90%; image-rendering: auto;" src="' + img + '" />';
-                            a.object.activity.render().find(".full-start-new__title").html(html);
+        Lampa.Listener.follow("full", function(a) {
+            if ("complite" == a.type && "1" != Lampa.Storage.get("logo_glav")) {
+                var e = a.data.movie;
+                var t = Lampa.TMDB.api(e.name ? "tv" : "movie/" + e.id + "/images?api_key=" + Lampa.TMDB.key() + "&language=" + Lampa.Storage.get("language"));
+                console.log(t);
+                $.get(t, function(e) {
+                    if (e.logos && e.logos[0]) {
+                        var t = e.logos[0].file_path;
+                        if ("" != t) {
+                            a.object.activity.render().find(".full-start-new__title").html(
+                                '<img style="margin-top: 5px;max-height: 125px;" src="' + 
+                                Lampa.TMDB.image("/t/p/w300" + t.replace(".svg", ".png")) + 
+                                '" />'
+                            );
                         }
                     }
                 });
