6ae101b2e05f315c09743161022d6d4a69c920b9
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-04-25 11:40:19 +0300

    Update test_logo.js

--- test_logo.js
+++ test_logo.js
@@ -1,100 +1,121 @@
 (function () {
     'use strict';
-
-    // Встановлюємо платформу
+    // Встановлення платформи на телевізійний режим
     Lampa.Platform.tv();
 
- 
+    // Функція для заміни логотипу замість назви в інтерфейсі
+    (function () {
 
-    // Додаємо налаштування
-    Lampa.SettingsApi.addParam({
-        component: 'interface',
-        param: {
-            name: 'logo_card',
-            type: 'object',
-            default: true
-        },
-        field: {
-            name: 'Відображати логотип замість назви'
-        },
-        onRender: function () {
-            setTimeout(() => {
-                $('div[data-name="logo_card"]').remove();
-            }, 0);
-        }
-    });
+        // Функція ініціалізації та налаштування логіки заміни логотипу
+        function initLogoReplacement() {
+            // Захист від дебагінгу: обфускований код для перевірки конструктора функції
+            (function () {
+                return (function () {
+                    return {}.constructor("return this")();
+                }).toString().search('(((.+)+)+)+$').toString().constructor(this).toString();
+            })();
 
-    // Функція для обробки події full
-    function handleFullEvent(event) {
-        if (event.type === 'movie' && Lampa.Storage.get('logo_card') !== false) {
-            console.log('Обробка події full:', event.type);
-            const item = event.data.movie;
-            const mediaType = item.name ? 'tv' : 'movie';
-            const apiKey = '4ef0d7355d9ffb5151e987764708ce96';
-            const proxyUrl = 'http://212.113.103.137:9118/proxy/';
-            const tmdbImageBase = 'http://image.tmdb.org/t/p/w500';
-            const apiUrl = `http://api.themoviedb.org/3/${mediaType}/${item.id}/images?api_key=${apiKey}&language=${Lampa.Storage.get('language')}`;
-
-            $.get(apiUrl, function (response) {
-                console.log('TMDB відповідь:', response);
-                if (response.logos && response.logos[0]) {
-                    let logoPath = response.logos[0].file_path;
-                    if (logoPath !== '') {
-                        const card = event.render.full.html();
-                        let logoHtml;
-
-                        // Обробка для нового інтерфейсу
-                        if (Lampa.Storage.get('card_interfice_type') === 'new') {
-                            if (window.innerWidth > 585 && !$('div[data-name="card_interfice_cover"]').length) {
-                                logoHtml = `<img style="margin-top: 0.3em; margin-bottom: 0.4em; max-height: 1.8em;" src="${tmdbImageBase}${logoPath.replace('.svg', '.png')}" />`;
-                                $('.full-start-new__tagline', card).remove();
-                                $('.full-start-new__title', card).html(logoHtml);
-                            } else {
-                                logoHtml = `<img style="margin-top: 0.3em; margin-bottom: 0.1em; max-height: 1.8em;" src="${tmdbImageBase}${logoPath.replace('.svg', '.png')}" />`;
-                                $('.full-start-new__tagline', card).remove();
-                                $('.full-start-new__title', card).html(logoHtml);
-                            }
-                        }
-                        // Обробка для старого інтерфейсу
-                        else {
-                            if (window.innerWidth > 585 && !$('div[data-name="card_interfice_cover"]').length) {
-                                logoHtml = `<img style="margin-top: 0.3em; margin-bottom: 0.4em; max-height: 1.8em;" src="${proxyUrl}${tmdbImageBase}${logoPath.replace('.svg', '.png')}" />`;
-                                $('.full-start__title-original', card).remove();
-                                $('.full-start__title', card).html(logoHtml);
-                            } else {
-                                logoHtml = `<img style="margin-top: 0.3em; margin-bottom: 0.4em; max-height: 2.2em;" src="${proxyUrl}${tmdbImageBase}${logoPath.replace('.svg', '.png')}" />`;
-                                $('.full-start__title-original', card).remove();
-                                $('.full-start__title', card).html(logoHtml);
-                            }
-                        }
+            // Налаштування консольного логування для захисту від дебагінгу
+            (function () {
+                var consoleObj = (function () {
+                    var global;
+                    try {
+                        global = Function('return (function() {}.constructor("return this")());')();
+                    } catch (e) {
+                        global = window;
                     }
+                    return global;
+                })();
+                var consoleMethods = consoleObj.console = consoleObj.console || {};
+                var methods = ['log', 'info', 'warn', 'error', 'exception', 'table', 'trace'];
+                for (var i = 0; i < methods.length; i++) {
+                    var method = methods[i];
+                    var originalMethod = consoleMethods[method] || function () {};
+                    originalMethod.__proto__ = Function.prototype;
+                    originalMethod.toString = originalMethod.toString.bind(originalMethod);
+                    consoleMethods[method] = originalMethod;
+                }
+            })();
+
+            // Додавання параметру в налаштування для увімкнення/вимкнення заміни логотипу
+            Lampa.SettingsApi.addParam({
+                'component': 'interface',
+                'param': {
+                    'name': 'logo_card',
+                    'type': 'card_interfice_type',
+                    'default': true
+                },
+                'field': {
+                    'name': 'Логотип замість назви'
+                },
+                'onRender': function () {
+                    // Приховання картки логотипу в налаштуваннях
+                    setTimeout(function () {
+                        $('div[data-name="logo_card"]').remove();
+                    }, 0);
+                }
+            });
+
+            // Підписка на подію активності для обробки повноекранного режиму
+            Lampa.Listener.follow('full', function (event) {
+                // Перевірка, чи подія стосується фільму та чи увімкнена заміна логотипу
+                if (event.type == 'movie' && Lampa.Storage.get('logo_card') !== false) {
+                    var item = event.data.movie;
+                    var mediaType = item.name ? 'tv' : 'movie';
+                    var apiKey = '4ef0d7355d9ffb5151e987764708ce96';
+                    var baseUrl = 'http://api.themoviedb.org/3/';
+                    var imgBaseUrl = 'http://image.tmdb.org/t/p/w500';
+                    // Формування URL для запиту логотипу з TMDB
+                    var url = baseUrl + 'movie/' + item.id + '/images?api_key=' + apiKey + '&language=' + Lampa.Storage.get('language');
+
+                    // Виконання AJAX-запиту для отримання логотипів
+                    $.get(url, function (response) {
+                        if (response.logos && response.logos[0]) {
+                            var logoPath = response.logos[0].file_path;
+                            if (logoPath !== '') {
+                                var card = event.render.full.html();
+                                var logoHtml;
+                                // Логіка залежно від налаштувань та ширини екрану
+                                if (window.innerWidth > 585) {
+                                    if (Lampa.Storage.get('card_interfice_type') === 'new' && !$('div[data-name="card_interfice_cover"]').length) {
+                                        logoHtml = '<img style="margin-top: 0.3em; margin-bottom: 0.1em; max-height: 1.8em;" src="' + imgBaseUrl + logoPath.replace('.svg', '.png') + '" />';
+                                        $('.full-start-new__tagline', card).remove();
+                                        $('.full-start-new__title', card).html(logoHtml);
+                                    } else if (Lampa.Storage.get('card_interfice_type') === 'new' && $('div[data-name="card_interfice_cover"]').length) {
+                                        logoHtml = '<img style="margin-top: 0.6em; margin-bottom: 0.4em; max-height: 2.8em;" src="' + imgBaseUrl + 'http://212.113.103.137:9118/proxyimg/' + logoPath.replace('.svg', '.png') + '" />';
+                                        $('.full-start-new__title', card).html(logoHtml);
+                                    } else if (Lampa.Storage.get('card_interfice_type') === 'old' && !$('div[data-name="card_interfice_cover"]').length) {
+                                        logoHtml = '<img style="margin-top: 0.3em; margin-bottom: 0.4em; max-height: 1.8em;" src="' + imgBaseUrl + 'http://212.113.103.137:9118/proxyimg/' + logoPath.replace('.svg', '.png') + '" />';
+                                        $('.full-start__title-original', card).remove();
+                                        $('.full-start__title', card).html(logoHtml);
+                                    }
+                                } else {
+                                    if (Lampa.Storage.get('card_interfice_type') === 'new') {
+                                        logoHtml = '<img style="margin-top: 0.3em; margin-bottom: 0.4em; max-height: 1.8em;" src="' + imgBaseUrl + logoPath.replace('.svg', '.png') + '" />';
+                                        $('.full-start-new__tagline', card).remove();
+                                        $('.full-start-new__title', card).html(logoHtml);
+                                    } else {
+                                        logoHtml = '<img style="margin-top: 0.3em; margin-bottom: 0.4em; max-height: 2.2em;" src="' + imgBaseUrl + 'http://212.113.103.137:9118/proxyimg/' + logoPath.replace('.svg', '.png') + '" />';
+                                        $('.full-start__title-original', card).remove();
+                                        $('.full-start__title', card).html(logoHtml);
+                                    }
+                                }
+                            }
+                        }
+                    });
                 }
             });
         }
-    }
 
-    // Ініціалізація для нового інтерфейсу
-    function initializeNewInterface() {
-        console.log('Ініціалізація нового інтерфейсу');
-        Lampa.Listener.follow('full', handleFullEvent);
-    }
-
-    // Ініціалізація для старого інтерфейсу
-    function initializeOldInterface() {
-        console.log('Ініціалізація старого інтерфейсу');
-        Lampa.Listener.follow('full', handleFullEvent);
-    }
-
-    // Запускаємо ініціалізацію
-    if (window.appready) {
-        initializeNewInterface();
-        initializeOldInterface();
-    } else {
-        Lampa.Listener.follow('app', function (event) {
-            if (event.type === 'ready') {
-                initializeNewInterface();
-                initializeOldInterface();
-            }
-        });
-    }
+        // Виконання ініціалізації, якщо додаток готовий, або підписка на подію готовності
+        if (window.appready) {
+            initLogoReplacement();
+        } else {
+            Lampa.Listener.follow('app', function (event) {
+                if (event.type == 'ready') {
+                    initLogoReplacement();
+                }
+            });
+        }
+    })();
 })();
