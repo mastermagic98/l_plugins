6689fca969908d290a8a08310912dfea08e9cec6
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-04-25 15:31:16 +0300

    Update test_logo_.js
    Працює, онолено для інших екранів

--- test_logo.js
+++ test_logo.js
@@ -47,9 +47,37 @@
                         if (logoPath !== '') {
                             var card = event.object.activity.render();
                             console.log('Title element found:', card.find('.full-start-new__title').length); // Лог для діагностики
-                            var logoHtml = '<img style="margin-top: 5px; max-height: 125px;" src="' + Lampa.TMDB.image('/t/p/w300' + logoPath.replace('.svg', '.png')) + '" />';
-                            card.find('.full-start-new__title').html(logoHtml);
-                            console.log('Rendered logo:', logoHtml); // Лог для діагностики
+                            var logoHtml;
+                            // Логіка залежно від налаштувань та ширини екрану
+                            if (window.innerWidth > 585) {
+                                if (Lampa.Storage.get('card_interfice_type') === 'new' && !card.find('div[data-name="card_interfice_cover"]').length) {
+                                    logoHtml = '<img style="margin-top: 0.3em; margin-bottom: 0.1em; max-height: 1.8em;" src="' + Lampa.TMDB.image('/t/p/w300' + logoPath.replace('.svg', '.png')) + '" />';
+                                    card.find('.full-start-new__tagline').remove();
+                                    card.find('.full-start-new__title').html(logoHtml);
+                                    console.log('Rendered logo (new, no cover, desktop):', logoHtml); // Лог для діагностики
+                                } else if (Lampa.Storage.get('card_interfice_type') === 'new' && card.find('div[data-name="card_interfice_cover"]').length) {
+                                    logoHtml = '<img style="margin-top: 0.6em; margin-bottom: 0.4em; max-height: 2.8em;" src="' + Lampa.TMDB.image('/t/p/w300' + logoPath.replace('.svg', '.png')) + 'http://212.113.103.137:9118/proxyimg/' + '" />';
+                                    card.find('.full-start-new__title').html(logoHtml);
+                                    console.log('Rendered logo (new, with cover, desktop):', logoHtml); // Лог для діагностики
+                                } else if (Lampa.Storage.get('card_interfice_type') === 'old' && !card.find('div[data-name="card_interfice_cover"]').length) {
+                                    logoHtml = '<img style="margin-top: 0.3em; margin-bottom: 0.4em; max-height: 1.8em;" src="' + Lampa.TMDB.image('/t/p/w300' + logoPath.replace('.svg', '.png')) + 'http://212.113.103.137:9118/proxyimg/' + '" />';
+                                    card.find('.full-start__title-original').remove();
+                                    card.find('.full-start__title').html(logoHtml);
+                                    console.log('Rendered logo (old, no cover, desktop):', logoHtml); // Лог для діагностики
+                                }
+                            } else {
+                                if (Lampa.Storage.get('card_interfice_type') === 'new') {
+                                    logoHtml = '<img style="margin-top: 0.3em; margin-bottom: 0.4em; max-height: 1.8em;" src="' + Lampa.TMDB.image('/t/p/w300' + logoPath.replace('.svg', '.png')) + '" />';
+                                    card.find('.full-start-new__tagline').remove();
+                                    card.find('.full-start-new__title').html(logoHtml);
+                                    console.log('Rendered logo (new, mobile):', logoHtml); // Лог для діагностики
+                                } else {
+                                    logoHtml = '<img style="margin-top: 0.3em; margin-bottom: 0.4em; max-height: 2.2em;" src="' + Lampa.TMDB.image('/t/p/w300' + logoPath.replace('.svg', '.png')) + 'http://212.113.103.137:9118/proxyimg/' + '" />';
+                                    card.find('.full-start__title-original').remove();
+                                    card.find('.full-start__title').html(logoHtml);
+                                    console.log('Rendered logo (old, mobile):', logoHtml); // Лог для діагностики
+                                }
+                            }
                         }
                     }
                 }).fail(function (xhr, status, error) {
