bcb71d0351ef39198462f64634a24e55a473e3cf
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-04-25 20:13:05 +0300

    Update test3.js
    остання робоча

--- test3.js
+++ test3.js
@@ -1,6 +1,5 @@
 (function () {
     'use strict';
-    console.log('Trailers Plugin: Loading...'); // Лог для перевірки завантаження
 
     var network = new Lampa.Reguest();
     var tmdb_api_key = Lampa.TMDB.key();
@@ -23,7 +22,7 @@
     function get(url, page, resolve, reject, useRegion) {
         var lang = Lampa.Storage.get('language', 'ru');
         var full_url = `${tmdb_base_url}${url}&api_key=${tmdb_api_key}&language=${lang}&page=${page}`;
-        if (useRegion) full_url += `&region=${getRegion()}`;
+        if (useRegion) full_url += `®ion=${getRegion()}`;
         console.log('API Request:', full_url);
         network.silent(full_url, function (result) {
             console.log('API Result:', url, result);
@@ -413,7 +412,7 @@
         this.cardImgBackground = function (card_data) {
             if (Lampa.Storage.field('background')) {
                 if (Lampa.Storage.get('background_type', 'complex') === 'poster' && window.innerWidth > 790) {
-                    return card_data.backdrop_path ? Lampa.Api.img(card_data.back perspective, Lampa.Api.img(card_data.backdrop_path, 'original') : '';
+                    return card_data.backdrop_path ? Lampa.Api.img(card_data.backdrop_path, 'original') : '';
                 }
                 return card_data.backdrop_path ? Lampa.Api.img(card_data.backdrop_path, 'w500') : '';
             }
@@ -450,7 +449,7 @@
             more.on('hover:focus', function (e) {
                 last = e.target;
                 scroll.update(more, true);
-            };
+            });
             scroll.append(more);
         };
 
@@ -880,180 +879,126 @@
     });
 
     function startPlugin() {
-        console.log('Trailers Plugin: Starting startPlugin'); // Лог для startPlugin
         window.plugin_trailers_ready = true;
-        console.log('Trailers Plugin: window.plugin_trailers_ready set to true');
-
-        try {
-            Lampa.Component.add('trailers_main', Component$1);
-            Lampa.Component.add('trailers_full', Component);
-            console.log('Trailers Plugin: Components added');
-        } catch (e) {
-            console.error('Trailers Plugin: Error adding components:', e.message);
-        }
-
-        try {
-            Lampa.Template.add('trailer', `
-                <div class="card selector card--trailer layer--render layer--visible">
-                    <div class="card__view">
-                        <img src="./img/img_load.svg" class="card__img">
-                        <div class="card__promo">
-                            <div class="card__promo-text">
-                                <div class="card__title"></div>
-                            </div>
-                            <div class="card__details"></div>
+        Lampa.Component.add('trailers_main', Component$1);
+        Lampa.Component.add('trailers_full', Component);
+        Lampa.Template.add('trailer', `
+            <div class="card selector card--trailer layer--render layer--visible">
+                <div class="card__view">
+                    <img src="./img/img_load.svg" class="card__img">
+                    <div class="card__promo">
+                        <div class="card__promo-text">
+                            <div class="card__title"></div>
                         </div>
-                    </div>
-                    <div class="card__play">
-                        <img src="./img/icons/player/play.svg">
+                        <div class="card__details"></div>
                     </div>
                 </div>
-            `);
-            Lampa.Template.add('trailer_style', `
-                <style>
-                .card.card--trailer,
-                .card-more.more--trailers {
-                    width: 25.7em;
-                }
-                .card.card--trailer .card__view {
-                    padding-bottom: 56%;
-                    margin-bottom: 0;
-                    position: relative;
-                }
-                .card.card--trailer .card__details {
-                    margin-top: 0.8em;
-                }
-                .card.card--trailer .card__play {
-                    position: absolute;
-                    top: 1.4em;
-                    left: 1.5em;
-                    background: #000000b8;
-                    width: 2.2em;
-                    height: 2.2em;
-                    border-radius: 100%;
-                    text-align: center;
-                    padding-top: 0.6em;
-                }
-                .card.card--trailer .card__play img {
-                    width: 0.9em;
-                    height: 1em;
-                }
-                .card.card--trailer .card__lang {
-                    position: absolute;
-                    top: 0.5em;
-                    right: 0.5em;
-                    background: #000000b8;
-                    color: white;
-                    padding: 0.2em 0.5em;
-                    border-radius: 3px;
-                    font-size: 0.9em;
-                }
-                .card-more.more--trailers .card-more__box {
-                    padding-bottom: 56%;
-                }
+                <div class="card__play">
+                    <img src="./img/icons/player/play.svg">
+                </div>
+            </div>
+        `);
+        Lampa.Template.add('trailer_style', `
+            <style>
+            .card.card--trailer,
+            .card-more.more--trailers {
+                width: 25.7em;
+            }
+            .card.card--trailer .card__view {
+                padding-bottom: 56%;
+                margin-bottom: 0;
+                position: relative;
+            }
+            .card.card--trailer .card__details {
+                margin-top: 0.8em;
+            }
+            .card.card--trailer .card__play {
+                position: absolute;
+                top: 1.4em;
+                left: 1.5em;
+                background: #000000b8;
+                width: 2.2em;
+                height: 2.2em;
+                border-radius: 100%;
+                text-align: center;
+                padding-top: 0.6em;
+            }
+            .card.card--trailer .card__play img {
+                width: 0.9em;
+                height: 1em;
+            }
+            .card.card--trailer .card__lang {
+                position: absolute;
+                top: 0.5em;
+                right: 0.5em;
+                background: #000000b8;
+                color: white;
+                padding: 0.2em 0.5em;
+                border-radius: 3px;
+                font-size: 0.9em;
+            }
+            .card-more.more--trailers .card-more__box {
+                padding-bottom: 56%;
+            }
+            .category-full--trailers .card {
+                margin-bottom: 1.5em;
+            }
+            .category-full--trailers .card {
+                width: 33.3%;
+            }
+            .items-line__filter {
+                display: inline-block;
+                margin-left: 10px;
+                cursor: pointer;
+            }
+            .items-line__filter svg {
+                width: 20px;
+                height: 20px;
+                vertical-align: middle;
+            }
+            @media screen and (max-width: 767px) {
                 .category-full--trailers .card {
-                    margin-bottom: 1.5em;
+                    width: 50%;
                 }
+            }
+            @media screen and (max-width: 400px) {
                 .category-full--trailers .card {
-                    width: 33.3%;
+                    width: 100%;
                 }
-                .items-line__filter {
-                    display: inline-block;
-                    margin-left: 10px;
-                    cursor: pointer;
-                }
-                .items-line__filter svg {
-                    width: 20px;
-                    height: 20px;
-                    vertical-align: middle;
-                }
-                @media screen and (max-width: 767px) {
-                    .category-full--trailers .card {
-                        width: 50%;
-                    }
-                }
-                @media screen and (max-width: 400px) {
-                    .category-full--trailers .card {
-                        width: 100%;
-                    }
-                }
-                </style>
-            `);
-            console.log('Trailers Plugin: Templates added');
-        } catch (e) {
-            console.error('Trailers Plugin: Error adding templates:', e.message);
-        }
+            }
+            </style>
+        `);
 
         function add() {
-            console.log('Trailers Plugin: Attempting to add menu item');
-            try {
-                var button = $(`
-                    <li class="menu__item selector">
-                        <div class="menu__ico">
-                            <svg height="70" viewBox="0 0 80 70" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" clip-rule="evenodd" d="M71.2555 2.08955C74.6975 3.2397 77.4083 6.62804 78.3283 10.9306C80 18.7291 80 35 80 35C80 35 80 51.2709 78.3283 59.0694C77.4083 63.372 74.6975 66.7603 71.2555 67.9104C65.0167 70 40 70 40 70C40 70 14.9833 70 8.74453 67.9104C5.3025 66.7603 2.59172 63.372 1.67172 59.0694C0 51.2709 0 35 0 35C0 35 0 18.7291 1.67172 10.9306C2.59172 6.62804 5.3025 3.2395 8.74453 2.08955C14.9833 0 40 0 40 0C40 0 65.0167 0 71.2555 2.08955ZM55.5909 35.0004L29.9773 49.5714V20.4286L55.5909 35.0004Z" fill="currentColor"/>
-                            </svg>
-                        </div>
-                        <div class="menu__text">${Lampa.Lang.translate('title_trailers')}</div>
-                    </li>
-                `);
-                button.on('hover:enter', function () {
-                    console.log('Trailers Plugin: Menu item clicked');
-                    Lampa.Activity.push({
-                        url: '',
-                        title: Lampa.Lang.translate('title_trailers'),
-                        component: 'trailers_main',
-                        page: 1
-                    });
+            var button = $(`
+                <li class="menu__item selector">
+                    <div class="menu__ico">
+                        <svg height="70" viewBox="0 0 80 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M71.2555 2.08955C74.6975 3.2397 77.4083 6.62804 78.3283 10.9306C80 18.7291 80 35 80 35C80 35 80 51.2709 78.3283 59.0694C77.4083 63.372 74.6975 66.7603 71.2555 67.9104C65.0167 70 40 70 40 70C40 70 14.9833 70 8.74453 67.9104C5.3025 66.7603 2.59172 63.372 1.67172 59.0694C0 51.2709 0 35 0 35C0 35 0 18.7291 1.67172 10.9306C2.59172 6.62804 5.3025 3.2395 8.74453 2.08955C14.9833 0 40 0 40 0C40 0 65.0167 0 71.2555 2.08955ZM55.5909 35.0004L29.9773 49.5714V20.4286L55.5909 35.0004Z" fill="currentColor"/>
+                        </svg>
+                    </div>
+                    <div class="menu__text">${Lampa.Lang.translate('title_trailers')}</div>
+                </li>
+            `);
+            button.on('hover:enter', function () {
+                Lampa.Activity.push({
+                    url: '',
+                    title: Lampa.Lang.translate('title_trailers'),
+                    component: 'trailers_main',
+                    page: 1
                 });
-                var menuList = $('.menu .menu__list').eq(0);
-                if (menuList.length) {
-                    menuList.append(button);
-                    console.log('Trailers Plugin: Menu item added successfully');
-                } else {
-                    console.error('Trailers Plugin: Menu list not found');
-                    setTimeout(add, 1000); // Повторити через 1 секунду
-                }
-                $('body').append(Lampa.Template.get('trailer_style', {}, true));
-                console.log('Trailers Plugin: Styles appended');
-            } catch (e) {
-                console.error('Trailers Plugin: Error adding menu item:', e.message);
-            }
+            });
+            $('.menu .menu__list').eq(0).append(button);
+            $('body').append(Lampa.Template.get('trailer_style', {}, true));
         }
 
-        console.log('Trailers Plugin: Checking Lampa and menu availability');
-        if (typeof Lampa !== 'undefined' && $('.menu .menu__list').length) {
-            console.log('Trailers Plugin: Lampa and menu ready, adding immediately');
-            add();
-        } else {
-            console.log('Trailers Plugin: Waiting for app ready');
-            try {
-                Lampa.Listener.follow('app', function (e) {
-                    if (e.type === 'ready') {
-                        console.log('Trailers Plugin: App ready, adding menu item');
-                        add();
-                    }
-                });
-            } catch (e) {
-                console.error('Trailers Plugin: Error setting app listener:', e.message);
-            }
-            // Додаткова спроба через таймер
-            setTimeout(function () {
-                if ($('.menu .menu__list').length) {
-                    console.log('Trailers Plugin: Menu found via timeout, adding');
-                    add();
-                } else {
-                    console.error('Trailers Plugin: Menu still not found after timeout');
-                }
-            }, 2000);
+        if (window.appready) add();
+        else {
+            Lampa.Listener.follow('app', function (e) {
+                if (e.type === 'ready') add();
+            });
         }
     }
 
-    console.log('Trailers Plugin: Checking if already initialized');
-    if (!window.plugin_trailers_ready) {
-        console.log('Trailers Plugin: Not initialized, starting');
-        startPlugin();
-    } else {
-        console.log('Trailers Plugin: Already initialized');
-    }
+    if (!window.plugin_trailers_ready) startPlugin();
 })();
