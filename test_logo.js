f66bd234035b58ecd3d01d130f45f159df1caeda
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-04-26 11:42:09 +0300

    Update test_logo.js
    для мобільних = 45

--- test_logo.js
+++ test_logo.js
@@ -59,7 +59,7 @@
             }
         });
 
-        // Додавання параметру для вибору режиму відображення
+        // Додавання параметру для вибору режиму відображення (залежить від logo_main)
         Lampa.SettingsApi.addParam({
             component: 'interface',
             param: {
@@ -73,7 +73,10 @@
             },
             field: {
                 name: Lampa.Lang.translate('logo_display_mode_title'),
-                description: Lampa.Lang.translate('logo_main_description')
+                description: Lampa.Lang.translate('logo_main_description'),
+                show: function () {
+                    return Lampa.Storage.get('logo_main') === '0';
+                }
             }
         });
 
@@ -136,7 +139,7 @@
                                 logoHtml = '<div><img style="display: block; margin-bottom: 0.2em; max-height: 2.8em;" src="' + Lampa.TMDB.image('/t/p/w500' + logoPath.replace('.svg', '.png')) + '" />' + (titleText ? '<span>' + titleText + '</span>' : '') + '</div>';
                                 card.find('.full-start-new__title').html(logoHtml);
                             } else if (Lampa.Storage.get('card_interfice_type') === 'old' && !card.find('div[data-name="card_interfice_cover"]').length) {
-                                logoHtml = '<div style="height: auto !important; overflow: visible !important;"><img style="display: block; margin-bottom: 0.2em; max-height: 1.8em;" src="' + Lampa.TMDB.image('/t/p/w300' + logoPath.replace('.svg', '.png')) + '" />' + (titleText ? '<span style="display: block; line-height: normal;">' + titleText + '</span>' : '') + '</div>';
+                                logoHtml = '<div style="height: auto !important; overflow: visible !important;"><img style="display: block; margin-bottom: 0.2em;" src="' + Lampa.TMDB.image('/t/p/w300' + logoPath.replace('.svg', '.png')) + '" onload="if(this.naturalHeight > 80) { let ratio = this.naturalWidth / this.naturalHeight; this.height = 80; this.width = 80 * ratio; }" />' + (titleText ? '<span style="display: block; line-height: normal;">' + titleText + '</span>' : '') + '</div>';
                                 card.find('.full-start__title-original').remove();
                                 card.find('.full-start__title').css({
                                     'height': 'auto !important',
@@ -150,7 +153,7 @@
                                 card.find('.full-start-new__tagline').remove();
                                 card.find('.full-start-new__title').html(logoHtml);
                             } else {
-                                logoHtml = '<div style="height: auto !important; overflow: visible !important;"><img style="display: block; margin-bottom: 0.2em; max-height: 2.2em;" src="' + Lampa.TMDB.image('/t/p/w300' + logoPath.replace('.svg', '.png')) + '" />' + (titleText ? '<span style="display: block; line-height: normal;">' + titleText + '</span>' : '') + '</div>';
+                                logoHtml = '<div style="height: auto !important; overflow: visible !important;"><img style="display: block; margin-bottom: 0.2em;" src="' + Lampa.TMDB.image('/t/p/w300' + logoPath.replace('.svg', '.png')) + '" onload="if(this.naturalHeight > 45) { let ratio = this.naturalWidth / this.naturalHeight; this.height = 45; this.width = 45 * ratio; }" />' + (titleText ? '<span style="display: block; line-height: normal;">' + titleText + '</span>' : '') + '</div>';
                                 card.find('.full-start__title-original').remove();
                                 card.find('.full-start__title').css({
                                     'height': 'auto !important',
