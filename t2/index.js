20be28202f7937044603fdfe9486bd33fed3a4f2
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-05-11 19:06:11 +0300

    Update index.js

--- t2/index.js
+++ t2/index.js
@@ -1,17 +1,1 @@
 // // Test1222
-
-                        Lampa.Storage.set('trailers_start', false);
-                        Lampa.Activity.push({
-                            url: '',
-                            title: Lampa.Lang.translate('trailers_main'),
-                            component: 'trailers_main',
-                            page: 1
-                        });
-                    }
-                }, 1000);
-            }
-        });
-    }
-
-    if (!window.plugin_trailers_ready) startPlugin();
-})();
