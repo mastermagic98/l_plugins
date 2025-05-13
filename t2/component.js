5c08592a83927d0c6cdacf84fd9c7533df6baacd
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-05-13 19:23:54 +0300

    Update component.js

--- t2/component.js
+++ t2/component.js
@@ -1,244 +1,236 @@
 (function () {
-    function component(object) {
-        var comp = this;
-        var scroll;
+    function ComponentMain(object) {
+        var scroll = new Lampa.Scroll({ mask: true, over: true, scroll_by_item: true });
         var items = [];
+        var html = $('<div></div>');
         var active = 0;
-        var light;
+        var light = Lampa.Storage.field('light_version') && window.innerWidth >= 767;
 
         this.create = function () {
-            scroll = $('<div class="upcoming scroll--h"></div>');
-            light = Lampa.Utils.isTouchDevice();
-            this.activity.loader(true);
-            this.build();
+            window.plugin_upcoming.Api.main(this.build.bind(this), this.empty.bind(this));
+            return this.render();
         };
 
-        this.build = function () {
-            var status = new Lampa.Status(5);
-            var results = {};
+        this.empty = function () {
+            var empty = new Lampa.Empty();
+            html.append(empty.render());
+            this.start = empty.start;
+            this.activity.loader(false);
+            this.activity.toggle();
+        };
 
-            status.onComplite = function () {
-                var hasItems = false;
-
-                for (var i in results) {
-                    if (results[i].results && results[i].results.length) {
-                        comp.append(results[i]);
-                        hasItems = true;
-                    }
-                }
-
-                if (!hasItems) scroll.append('<div class="upcoming__empty">' + Lampa.Lang.translate('upcoming_empty') + '</div>');
-
-                if (light) Lampa.Background.immediately('');
-
-                comp.activity.loader(false);
-                comp.activity.toggle();
-            };
-
-            var today = new Date();
-            var priorDate = new Date(new Date().setDate(today.getDate() - 30));
-            var dateString = priorDate.getFullYear() + '-' + (priorDate.getMonth() + 1).toString().padStart(2, '0') + '-' + priorDate.getDate().toString().padStart(2, '0');
-
-            Lampa.TMDB.api('discover/movie?region=' + (Lampa.Storage.get('region') || 'US') + '&language=' + (Lampa.Storage.get('language') || 'en') + '&sort_by=popularity.desc&release_date.gte=' + dateString + '&with_release_type=3|2', function (json) {
-                if (json.results) {
-                    var localStatus = new Lampa.Status(json.results.length);
-
-                    localStatus.onComplite = function () {
-                        results['in_theaters'] = {
-                            title: Lampa.Lang.translate('upcoming_in_theaters'),
-                            results: json.results,
-                            type: 'in_theaters'
-                        };
-                        status.append({}, {});
-                    };
-
-                    json.results.forEach(function (item, i) {
-                        Lampa.TMDB.release(item.id, 'movie', function (release) {
-                            json.results[i].release_details = release;
-                            localStatus.append(item.id, {});
-                        }, function () {
-                            localStatus.append(item.id, {});
-                        });
-                    });
-                } else {
-                    status.append({}, {});
-                }
-            }, function () {
-                status.append({}, {});
-            });
-
-            var nextMonth = new Date(new Date().setMonth(today.getMonth() + 1));
-            var dateString2 = today.getFullYear() + '-' + (today.getMonth() + 1).toString().padStart(2, '0') + '-' + today.getDate().toString().padStart(2, '0');
-            var endDateString = nextMonth.getFullYear() + '-' + (nextMonth.getMonth() + 1).toString().padStart(2, '0') + '-' + nextMonth.getDate().toString().padStart(2, '0');
-
-            Lampa.TMDB.api('discover/movie?region=' + (Lampa.Storage.get('region') || 'US') + '&language=' + (Lampa.Storage.get('language') || 'en') + '&sort_by=popularity.desc&release_date.gte=' + dateString2 + '&release_date.lte=' + endDateString, function (json) {
-                if (json.results) {
-                    var localStatus = new Lampa.Status(json.results.length);
-
-                    localStatus.onComplite = function () {
-                        results['upcoming'] = {
-                            title: Lampa.Lang.translate('upcoming_upcoming'),
-                            results: json.results,
-                            type: 'upcoming'
-                        };
-                        status.append({}, {});
-                    };
-
-                    json.results.forEach(function (item, i) {
-                        Lampa.TMDB.release(item.id, 'movie', function (release) {
-                            json.results[i].release_details = release;
-                            localStatus.append(item.id, {});
-                        }, function () {
-                            localStatus.append(item.id, {});
-                        });
-                    });
-                } else {
-                    status.append({}, {});
-                }
-            }, function () {
-                status.append({}, {});
-            });
-
-            Lampa.TMDB.api('discover/tv?region=' + (Lampa.Storage.get('region') || 'US') + '&language=' + (Lampa.Storage.get('language') || 'en') + '&sort_by=popularity.desc&first_air_date.gte=' + dateString, function (json) {
-                if (json.results) {
-                    var localStatus = new Lampa.Status(json.results.length);
-
-                    localStatus.onComplite = function () {
-                        results['series_new'] = {
-                            title: Lampa.Lang.translate('upcoming_series_new'),
-                            results: json.results,
-                            type: 'series_new'
-                        };
-                        status.append({}, {});
-                    };
-
-                    json.results.forEach(function (item, i) {
-                        Lampa.TMDB.release(item.id, 'tv', function (release) {
-                            json.results[i].release_details = release;
-                            localStatus.append(item.id, {});
-                        }, function () {
-                            localStatus.append(item.id, {});
-                        });
-                    });
-                } else {
-                    status.append({}, {});
-                }
-            }, function () {
-                status.append({}, {});
-            });
-
-            Lampa.TMDB.api('discover/tv?region=' + (Lampa.Storage.get('region') || 'US') + '&language=' + (Lampa.Storage.get('language') || 'en') + '&sort_by=popularity.desc&first_air_date.gte=' + dateString2 + '&first_air_date.lte=' + endDateString, function (json) {
-                if (json.results) {
-                    var localStatus = new Lampa.Status(json.results.length);
-
-                    localStatus.onComplite = function () {
-                        results['series_upcoming'] = {
-                            title: Lampa.Lang.translate('upcoming_series_upcoming'),
-                            results: json.results,
-                            type: 'series_upcoming'
-                        };
-                        status.append({}, {});
-                    };
-
-                    json.results.forEach(function (item, i) {
-                        Lampa.TMDB.release(item.id, 'tv', function (release) {
-                            json.results[i].release_details = release;
-                            localStatus.append(item.id, {});
-                        }, function () {
-                            localStatus.append(item.id, {});
-                        });
-                    });
-                } else {
-                    status.append({}, {});
-                }
-            }, function () {
-                status.append({}, {});
-            });
-
-            Lampa.TMDB.api('trending/all/week?language=' + (Lampa.Storage.get('language') || 'en'), function (json) {
-                if (json.results) {
-                    var localStatus = new Lampa.Status(json.results.length);
-
-                    localStatus.onComplite = function () {
-                        results['popular'] = {
-                            title: Lampa.Lang.translate('upcoming_popular'),
-                            results: json.results,
-                            type: 'popular'
-                        };
-                        status.append({}, {});
-                    };
-
-                    json.results.forEach(function (item, i) {
-                        Lampa.TMDB.release(item.id, item.media_type, function (release) {
-                            json.results[i].release_details = release;
-                            localStatus.append(item.id, {});
-                        }, function () {
-                            localStatus.append(item.id, {});
-                        });
-                    });
-                } else {
-                    status.append({}, {});
-                }
-            }, function () {
-                status.append({}, {});
-            });
+        this.build = function (data) {
+            var _this = this;
+            scroll.minus();
+            html.append(scroll.render());
+            data.forEach(this.append.bind(this));
+            if (light) {
+                scroll.onWheel = function (step) {
+                    if (step > 0) _this.down();
+                    else _this.up();
+                };
+            }
+            this.activity.loader(false);
+            this.activity.toggle();
         };
 
         this.append = function (element) {
-            var item = new window.plugin_upcoming.line(element);
+            var item = new window.plugin_upcoming.Line(element);
             item.create();
-
-            item.onDown = this.down;
-            item.onUp = this.up;
-            item.onBack = this.back;
-
+            item.onDown = this.down.bind(this);
+            item.onUp = this.up.bind(this);
+            item.onBack = this.back.bind(this);
             item.onToggle = function () {
                 active = items.indexOf(item);
             };
-
+            item.wrap = $('<div></div>');
             if (light) {
-                item.wrap = $('<div></div>');
                 scroll.append(item.wrap);
             } else {
                 scroll.append(item.render());
             }
-
             items.push(item);
-        };
-
-        this.down = function () {
-            active++;
-            if (active >= items.length) active = 0;
-            items[active].toggle();
-        };
-
-        this.up = function () {
-            active--;
-            if (active < 0) active = items.length - 1;
-            items[active].toggle();
         };
 
         this.back = function () {
             Lampa.Activity.backward();
         };
 
-        this.start = function () {
-            if (items.length) items[active].toggle();
+        this.detach = function () {
+            if (light) {
+                items.forEach(function (item) {
+                    item.render().detach();
+                });
+                items.slice(active, active + 2).forEach(function (item) {
+                    item.wrap.append(item.render());
+                });
+            }
         };
 
-        this.activity = object.activity;
+        this.down = function () {
+            active++;
+            active = Math.min(active, items.length - 1);
+            this.detach();
+            items[active].toggle();
+            scroll.update(items[active].render());
+        };
+
+        this.up = function () {
+            active--;
+            if (active < 0) {
+                active = 0;
+                this.detach();
+                Lampa.Controller.toggle('head');
+            } else {
+                this.detach();
+                items[active].toggle();
+            }
+            scroll.update(items[active].render());
+        };
+
+        this.start = function () {
+            var _this2 = this;
+            if (Lampa.Activity.active().activity !== this.activity) return;
+            Lampa.Controller.add('content', {
+                toggle: function () {
+                    if (items.length) {
+                        _this2.detach();
+                        items[active].toggle();
+                    }
+                },
+                left: function () {
+                    if (Navigator.canmove('left')) Navigator.move('left');
+                    else Lampa.Controller.toggle('menu');
+                },
+                right: function () {
+                    Navigator.move('right');
+                },
+                up: function () {
+                    if (Navigator.canmove('up')) Navigator.move('up');
+                    else Lampa.Controller.toggle('head');
+                },
+                down: function () {
+                    if (Navigator.canmove('down')) Navigator.move('down');
+                },
+                back: this.back
+            });
+            Lampa.Controller.toggle('content');
+        };
+
+        this.pause = function () {};
+        this.stop = function () {};
+        this.render = function () {
+            return html;
+        };
 
         this.destroy = function () {
-            items.forEach(function (item) {
-                item.destroy();
-            });
+            Lampa.Arrays.destroy(items);
+            scroll.destroy();
+            html.remove();
             items = [];
-            if (scroll) scroll.remove();
         };
-
-        Lampa.Component.add('upcoming', this);
     }
 
-    window.plugin_upcoming = window.plugin_upcoming || {};
-    window.plugin_upcoming.component = component;
-})();
+    function ComponentFull(object) {
+        var scroll = new Lampa.Scroll({ mask: true, over: true, step: 250, end_ratio: 2 });
+        var items = [];
+        var html = $('<div></div>');
+        var body = $('<div class="category-full category-full--trailers"></div>');
+        var newlampa = Lampa.Manifest.app_digital >= 166;
+        var light = newlampa ? false : Lampa.Storage.field('light_version') && window.innerWidth >= 767;
+        var total_pages = 0;
+        var last;
+        var waitload = false;
+        var active = 0;
+
+        this.create = function () {
+            window.plugin_upcoming.Api.full(object, this.build.bind(this), this.empty.bind(this));
+            return this.render();
+        };
+
+        this.empty = function () {
+            var empty = new Lampa.Empty();
+            scroll.append(empty.render());
+            this.start = empty.start;
+            this.activity.loader(false);
+            this.activity.toggle();
+        };
+
+        this.next = function () {
+            var _this = this;
+            if (waitload) return;
+            if (object.page < total_pages && object.page < 30) {
+                waitload = true;
+                object.page++;
+                window.plugin_upcoming.Api.full(object, function (result) {
+                    if (result.results && result.results.length) {
+                        _this.append(result, true);
+                    } else {
+                        Lampa.Noty.show(Lampa.Lang.translate('trailers_no_trailers'));
+                    }
+                    waitload = false;
+                }, function () {
+                    Lampa.Noty.show(Lampa.Lang.translate('trailers_no_trailers'));
+                    waitload = false;
+                });
+            } else {
+                Lampa.Noty.show(Lampa.Lang.translate('trailers_no_more_data'));
+            }
+        };
+
+        this.cardImgBackground = function (card_data) {
+            if (Lampa.Storage.field('background')) {
+                if (Lampa.Storage.get('background_type', 'complex') === 'poster' && window.innerWidth > 790) {
+                    return card_data.backdrop_path ? Lampa.Api.img(card_data.backdrop_path, 'original') : '';
+                }
+                return card_data.backdrop_path ? Lampa.Api.img(card_data.backdrop_path, 'w500') : '';
+            }
+            return '';
+        };
+
+        this.append = function (data, append) {
+            var _this2 = this;
+            if (!append) body.empty();
+            data.results.forEach(function (element) {
+                var card = new window.plugin_upcoming.Trailer(element, { type: object.type });
+                card.create();
+                card.visible();
+                card.onFocus = function (target, card_data) {
+                    last = target;
+                    scroll.update(card.render(), true);
+                    if (!light && !newlampa && scroll.isEnd()) _this2.next();
+                    if (items.length > 0 && items.indexOf(card) === items.length - 1) {
+                        var message = Lampa.Lang.translate('trailers_last_movie').replace('[title]', card_data.title || card_data.name);
+                        Lampa.Noty.show(message);
+                    }
+                };
+                body.append(card.render());
+                items.push(card);
+            });
+            var cardCount = data.results.length;
+            if (cardCount < 20) {
+                for (var i = cardCount; i < 20; i++) {
+                    var placeholder = $('<div class="card card--placeholder" style="width: 33.3%; margin-bottom: 1.5em; visibility: hidden;"></div>');
+                    body.append(placeholder);
+                }
+            }
+        };
+
+        this.build = function (data) {
+            var _this3 = this;
+            if (data.results && data.results.length) {
+                total_pages = data.total_pages || 1;
+                scroll.minus();
+                html.append(scroll.render());
+                this.append(data);
+                if (light && items.length) this.back();
+                if (total_pages > data.page && items.length) {
+                    this.more();
+                }
+                scroll.append(body);
+                if (newlampa) {
+                    scroll.onEnd = this.next.bind(this);
+                    scroll.onWheel = function (step) {
+                        if (!Lampa.Controller.own(_this3)) _this3.start();
+                        if (step > 0) Navigator.move('down');
+                        else if (active >
