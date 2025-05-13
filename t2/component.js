3980d5f5586742bf621eeb5ba1c2f5129da9268b
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-05-13 17:22:01 +0300

    Update component.js

--- t2/component.js
+++ t2/component.js
@@ -22,24 +22,32 @@ function Component(object) {
             Lampa.Component.add('trailers', this);
             this.build();
         } catch (e) {
-            console.error('Error in Component.create:', e); // Діагностика
-            throw e;
+            console.error('Error in Component.create:', e);
+            scroll.append('<div class="trailers__empty">' + Lampa.Lang.translate('trailers_empty') + '</div>');
         }
     };
 
     this.build = function () {
         console.log('Component.build called'); // Діагностика
         try {
+            if (!Lampa.Status) {
+                console.error('Lampa.Status is undefined');
+                scroll.append('<div class="trailers__empty">' + Lampa.Lang.translate('trailers_empty') + '</div>');
+                return;
+            }
             var status = new Lampa.Status(5);
             var results = {};
             status.onComplite = function () {
                 console.log('Status completed:', results); // Діагностика
+                var hasItems = false;
                 for (var i in results) {
-                    if (results[i].results.length) {
+                    if (results[i].results && results[i].results.length) {
                         this.append(results[i]);
+                        hasItems = true;
                     }
                 }
-                if (!items.length) {
+                if (!hasItems) {
+                    console.log('No items to display'); // Діагностика
                     scroll.append('<div class="trailers__empty">' + Lampa.Lang.translate('trailers_empty') + '</div>');
                 }
                 if (light) Lampa.Background.immediately('');
@@ -48,31 +56,40 @@ function Component(object) {
             }.bind(this);
             main(status, results);
         } catch (e) {
-            console.error('Error in Component.build:', e); // Діагностика
-            throw e;
+            console.error('Error in Component.build:', e);
+            scroll.append('<div class="trailers__empty">' + Lampa.Lang.translate('trailers_empty') + '</div>');
         }
     };
 
     this.append = function (element) {
         console.log('Component.append called:', element); // Діагностика
-        var item = new Line(element);
-        item.create();
-        item.onDown = this.down.bind(this);
-        item.onUp = this.up.bind(this);
-        item.onBack = this.back.bind(this);
-        item.onToggle = function () {
-            active = items.indexOf(item);
-        };
-        item.wrap = $('<div></div>');
-        if (light) {
-            scroll.append(item.wrap);
-        } else {
-            scroll.append(item.render());
+        try {
+            var item = new Line(element);
+            item.create();
+            item.onDown = this.down.bind(this);
+            item.onUp = this.up.bind(this);
+            item.onBack = this.back.bind(this);
+            item.onToggle = function () {
+                active = items.indexOf(item);
+            };
+            item.wrap = $('<div></div>');
+            if (light) {
+                scroll.append(item.wrap);
+            } else {
+                scroll.append(item.render());
+            }
+            items.push(item);
+        } catch (e) {
+            console.error('Error in Component.append:', e);
         }
-        items.push(item);
     };
 
-    // ... (інші методи: down, up, back, start, activity, destroy)
+    this.down = function () { /* ... */ };
+    this.up = function () { /* ... */ };
+    this.back = function () { /* ... */ };
+    this.start = function () { /* ... */ };
+    this.activity = object.activity;
+    this.destroy = function () { /* ... */ };
 }
 
 export { Component };
