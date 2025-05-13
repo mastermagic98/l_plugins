350eeef3669a8034c013cc08da064eb6e2465a9d
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-05-13 18:09:25 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
         library: {
-            type: 'commonjs', // Змінено на commonjs для стабільності
+            name: 'LampaPlugin', // Глобальна змінна для плагіна
+            type: 'var', // Вивід як глобальна змінна
         },
     },
     module: {
