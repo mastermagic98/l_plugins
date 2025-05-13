46f6640a9e93ff205f018e4b6e99e87f8075a871
GitHub Action <action@github.com>
2025-05-13 14:38:56 +0000

    Auto-generate dist/t2.js

--- dist/t2.js
+++ dist/t2.js
@@ -44,7 +44,7 @@ __webpack_require__.r(__webpack_exports__);
 __webpack_require__.d(__webpack_exports__, {
   Api: () => (/* reexport */ Api),
   Component: () => (/* reexport */ Component),
-  startPlugin: () => (/* binding */ startPlugin)
+  initPlugin: () => (/* binding */ initPlugin)
 });
 
 ;// ./t2/utils.js
@@ -627,43 +627,47 @@ function Component(object) {
 ;// ./t2/index.js
 
 
-function startPlugin() {
-  console.log('startPlugin called'); // Діагностика
-  if (typeof Lampa === 'undefined' || !Lampa.Status || !Lampa.TMDB) {
-    console.error('Lampa API not ready, retrying...');
-    setTimeout(startPlugin, 100); // Повторна спроба через 100 мс
-    return;
-  }
-  console.log('Lampa API ready:', Lampa.Status, Lampa.TMDB); // Діагностика
-  Lampa.Lang.add({
-    // ... (переклади)
-  });
-  Lampa.Plugin.add({
-    url: '',
-    title: Lampa.Lang.translate('title_trailers'),
-    icon: '<svg>...</svg>',
-    in_menu: true,
-    in_cub: true,
-    status: 1,
-    start: function () {
-      console.log('Plugin start triggered'); // Діагностика
-      Lampa.Activity.push({
+function initPlugin() {
+  console.log('initPlugin called');
+  Lampa.Listener.follow('app', function (e) {
+    if (e.type === 'app_ready') {
+      console.log('Lampa app ready:', Lampa.Status, Lampa.TMDB);
+      if (!Lampa.Status || !Lampa.TMDB) {
+        console.error('Lampa API not available');
+        return;
+      }
+      Lampa.Lang.add({
+        trailers_in_theaters: {
+          ru: 'В кинотеатрах',
+          uk: 'У кінотеатрах',
+          en: 'In Theaters'
+        }
+        // ... (інші переклади)
+      });
+      Lampa.Plugin.add({
         url: '',
         title: Lampa.Lang.translate('title_trailers'),
-        component: 'trailers',
-        page: 1
+        icon: '<svg>...</svg>',
+        in_menu: true,
+        in_cub: true,
+        status: 1,
+        start: function () {
+          console.log('Plugin start triggered');
+          Lampa.Activity.push({
+            url: '',
+            title: Lampa.Lang.translate('title_trailers'),
+            component: 'trailers',
+            page: 1
+          });
+        }
       });
-    }
-  });
-  Lampa.Listener.follow('app', function (e) {
-    if (e.type == 'app_ready') {
       Lampa.Template.add('trailers', '<div>...</div>');
       Lampa.Template.add('trailer', '<div>...</div>');
       Lampa.Template.add('line', '<div>...</div>');
     }
   });
 }
-startPlugin();
+initPlugin();
 
 var __webpack_export_target__ = exports;
 for(var __webpack_i__ in __webpack_exports__) __webpack_export_target__[__webpack_i__] = __webpack_exports__[__webpack_i__];
