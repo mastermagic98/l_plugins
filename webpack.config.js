ec9f9b577c99788a065226f1f91d0051e698ec81
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-05-13 18:43:40 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -35,11 +35,7 @@ module.exports = {
     },
     optimization: {
         minimize: false,
-        concatenateModules: true,
     },
     mode: 'development',
     devtool: 'source-map',
-    resolve: {
-        modules: [path.resolve(__dirname, 't2'), 'node_modules'], // Явно вказуємо папку t2
-    },
 };
