07f380aa71ff8a13b90bb86b18bd0c044d2fa6d1
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-05-14 09:00:16 +0300

    Update line.js

--- t2/line.js
+++ t2/line.js
@@ -1,45 +1,54 @@
 (function () {
-    function line(data) {
+    function Line(params) {
         var _this = this;
-
-        this.element;
-
-        this.create = function () {
-            _this.element = $('<div class="line"><div class="line__title">' + Lampa.Utils.shortText(data.title, 34) + '</div><div class="line__cards scroll--h"></div></div>');
-
-            data.results.forEach(function (item) {
-                var card = new window.plugin_upcoming.trailer(item);
-                card.create();
-                var rendered = card.render();
-
-                if (rendered) _this.element.find('.line__cards').append(rendered);
+        this.params = params;
+        this.cards = [];
+        this.container = $('<div class="items-line category-full category-full--trailers"></div>');
+        this.more = $('<div class="card-more more--trailers selector"><div class="card-more__box"><div class="card-more__title">' + Lampa.Lang.translate('trailers_more') + '</div></div></div>');
+        this.container.append(this.more);
+        this.page = 1;
+        this.load = window.plugin_upcoming.utils.debounce(function () {
+            _this.loading(true);
+            window.plugin_upcoming.Api.full({
+                category: _this.params.category,
+                page: _this.page
+            }, function (data) {
+                _this.loading(false);
+                data.results.forEach(function (item) {
+                    var card = new window.plugin_upcoming.Trailer(item, _this.params);
+                    _this.cards.push(card);
+                    _this.container.append(card.card);
+                });
+                if (_this.page >= data.total_pages) {
+                    _this.more.hide();
+                    if (data.results.length === 1) {
+                        var title = data.results[0].title;
+                        Lampa.Noty.show(Lampa.Lang.translate('trailers_last_movie').replace('[title]', title));
+                    } else if (!data.results.length) {
+                        Lampa.Noty.show(Lampa.Lang.translate('trailers_no_more_data'));
+                    }
+                }
+            }, function () {
+                _this.loading(false);
+                Lampa.Noty.show(Lampa.Lang.translate('trailers_no_trailers'));
             });
-
-            _this.element.find('.line__cards .trailer').on('hover:focus', function (e, data) {
-                scroll.update($(this), true);
-                Lampa.Background.change(Lampa.TMDB.image('backdrop_path', data.backdrop_path, true));
-            });
-        };
-
-        this.toggle = function () {
-            var cards = _this.element.find('.line__cards .trailer');
-
-            if (cards.length) {
-                Lampa.Controller.enabled().collectionSet(_this.element.find('.line__cards'));
-                Lampa.Controller.collectionFocus(cards.eq(0), _this.element.find('.line__cards'));
-            }
-        };
-
-        this.render = function () {
-            return _this.element;
-        };
-
-        this.destroy = function () {
-            if (_this.element) _this.element.remove();
-            _this.element = null;
-        };
+        }, 100);
+        this.more.on('hover:enter', function () {
+            _this.page++;
+            _this.load();
+        });
+        this.loading(false);
+        this.load();
     }
 
+    Line.prototype.loading = function (status) {
+        if (status) {
+            this.container.addClass('loading');
+        } else {
+            this.container.removeClass('loading');
+        }
+    };
+
     window.plugin_upcoming = window.plugin_upcoming || {};
-    window.plugin_upcoming.line = line;
+    window.plugin_upcoming.Line = Line;
 })();
