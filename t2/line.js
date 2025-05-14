2d5fe05e8677576d78dc472f37dca77faa06da46
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-05-14 11:08:57 +0300

    Update line.js

--- t2/line.js
+++ t2/line.js
@@ -7,49 +7,73 @@
         this.count = 20;
         this.more = $('<div class="card-more more--trailers"><div class="card-more__box"><div class="card-more__text">' + Lampa.Lang.translate('trailers_more') + '</div></div></div>');
         console.log('[Line] Initialized with params:', params);
-        this.container.append(this.more);
-        this.page = 1;
-        this.load = window.plugin_upcoming.utils.debounce(function () {
-            _this.loading(true);
-            window.plugin_upcoming.Api.full({
-                category: _this.params.category,
-                page: _this.page
-            }, function (data) {
-                _this.loading(false);
-                data.results.forEach(function (item) {
-                    var card = new window.plugin_upcoming.Trailer(item, _this.params);
-                    _this.cards.push(card);
-                    _this.container.append(card.card);
-                });
-                if (_this.page >= data.total_pages) {
-                    _this.more.hide();
-                    if (data.results.length === 1) {
-                        var title = data.results[0].title;
-                        Lampa.Noty.show(Lampa.Lang.translate('trailers_last_movie').replace('[title]', title));
-                    } else if (!data.results.length) {
-                        Lampa.Noty.show(Lampa.Lang.translate('trailers_no_more_data'));
-                    }
-                }
-            }, function () {
-                _this.loading(false);
-                Lampa.Noty.show(Lampa.Lang.translate('trailers_no_trailers'));
-            });
-        }, 100);
-        this.more.on('hover:enter', function () {
-            _this.page++;
-            _this.load();
-        });
-        this.loading(false);
-        this.load();
-    }
 
-    Line.prototype.loading = function (status) {
-        if (status) {
-            this.container.addClass('loading');
-        } else {
-            this.container.removeClass('loading');
-        }
-    };
+        this.load = function () {
+            if (this.params.category) {
+                var url = Lampa.TMDB.url(this.params.type + '/' + this.params.category);
+                var language = Lampa.Storage.get('language', 'uk');
+                var region = language === 'uk' ? 'UA' : language === 'ru' ? 'RU' : 'US';
+
+                if (this.params.filter) {
+                    url = Lampa.TMDB.url('trending/' + this.params.type + '/' + this.params.filter);
+                }
+
+                Lampa.TMDB.get(
+                    url,
+                    {
+                        language: language,
+                        region: region,
+                        page: this.page
+                    },
+                    function (data) {
+                        if (data.results && data.results.length) {
+                            data.results.forEach(function (item) {
+                                var card = new window.plugin_upcoming.Trailer(item, this.params);
+                                this.cards.push(card);
+                                this.container.append(card.card);
+                            }.bind(this));
+
+                            if (data.results.length < this.count) {
+                                this.more.hide();
+                            } else {
+                                this.container.append(this.more);
+                            }
+                        } else {
+                            this.more.hide();
+                            if (this.page === 1) {
+                                Lampa.Noty.show(Lampa.Lang.translate('trailers_no_trailers'));
+                            }
+                        }
+
+                        if (!data.results.length && this.page > 1) {
+                            Lampa.Noty.show(Lampa.Lang.translate('trailers_no_more_data'));
+                        }
+
+                        this.page++;
+                    }.bind(this),
+                    function () {
+                        this.more.hide();
+                        Lampa.Noty.show(Lampa.Lang.translate('trailers_no_trailers'));
+                    }.bind(this)
+                );
+            }
+        }.bind(this);
+
+        this.load();
+
+        this.more.on('hover:enter', function () {
+            this.load();
+        }.bind(this));
+
+        this.destroy = function () {
+            this.cards.forEach(function (c) {
+                c.card.remove();
+            });
+            this.cards = [];
+            this.container.remove();
+            this.more.remove();
+        };
+    }
 
     window.plugin_upcoming = window.plugin_upcoming || {};
     window.plugin_upcoming.Line = Line;
