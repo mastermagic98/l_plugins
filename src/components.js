4dd09bec212a61fcba71364c117cbc9ec5545a8d
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-05-17 09:54:05 +0300

    Update components.js

--- src/components.js
+++ src/components.js
@@ -1,144 +1,150 @@
-var Component = {
-    render_cards: function (data, params) {
-        var cards = [];
-        var lang = Lampa.Storage.get('language', 'ru');
+(function() {
+    var Component = {
+        render_cards: function (data, params) {
+            var cards = [];
+            var lang = Lampa.Storage.get('language', 'ru');
 
-        data.forEach(function (item) {
-            var results = item.results || [];
-            results.forEach(function (card) {
-                var is_movie = !card.name;
-                var title = is_movie ? (card.title || '') : (card.name || '');
-                var original_title = is_movie ? (card.original_title || '') : (card.original_name || '');
-                var date = '';
+            data.forEach(function (item) {
+                var results = item.results || [];
+                results.forEach(function (card) {
+                    var is_movie = !card.name;
+                    var title = is_movie ? (card.title || '') : (card.name || '');
+                    var original_title = is_movie ? (card.original_title || '') : (card.original_name || '');
+                    var date = '';
 
-                if (card.release_details) {
-                    if (is_movie && card.release_details.release_date) {
-                        date = card.release_details.release_date;
-                    } else if (!is_movie && card.release_details.first_air_date) {
-                        date = card.release_details.first_air_date;
+                    if (card.release_details) {
+                        if (is_movie && card.release_details.release_date) {
+                            date = card.release_details.release_date;
+                        } else if (!is_movie && card.release_details.first_air_date) {
+                            date = card.release_details.first_air_date;
+                        }
                     }
-                }
-                if (!date && card.release_date) {
-                    date = card.release_date;
-                }
-                if (!date && card.first_air_date) {
-                    date = card.first_air_date;
-                }
-
-                if (date) {
-                    try {
-                        date = Lampa.Utils.dateFormat(new Date(date), lang === 'ru' ? 'DD.MM.YYYY' : lang === 'uk' ? 'DD.MM.YYYY' : 'MM/DD/YYYY');
-                    } catch (e) {
-                        console.log('Trailers', 'Date parse error:', card.id, date);
-                        date = '';
+                    if (!date && card.release_date) {
+                        date = card.release_date;
                     }
-                }
+                    if (!date && card.first_air_date) {
+                        date = card.first_air_date;
+                    }
 
-                console.log('Trailers', 'Card:', card.id, 'Title:', title, 'Date:', date);
+                    if (date) {
+                        try {
+                            date = Lampa.Utils.dateFormat(new Date(date), lang === 'ru' ? 'DD.MM.YYYY' : lang === 'uk' ? 'DD.MM.YYYY' : 'MM/DD/YYYY');
+                        } catch (e) {
+                            console.log('Trailers', 'Date parse error:', card.id, date);
+                            date = '';
+                        }
+                    }
 
-                var img = card.poster_path ? 'https://image.tmdb.org/t/p/w300' + card.poster_path : './img/poster.png';
-                var card_data = {
-                    id: card.id,
-                    type: is_movie ? 'movie' : 'tv',
-                    title: title,
-                    original_title: original_title,
-                    img: img,
-                    date: date,
-                    category_type: item.type,
-                    category_url: item.url
-                };
-                cards.push(card_data);
+                    console.log('Trailers', 'Card:', card.id, 'Title:', title, 'Date:', date);
+
+                    var img = card.poster_path ? 'https://image.tmdb.org/t/p/w300' + card.poster_path : './img/poster.png';
+                    var card_data = {
+                        id: card.id,
+                        type: is_movie ? 'movie' : 'tv',
+                        title: title,
+                        original_title: original_title,
+                        img: img,
+                        date: date,
+                        category_type: item.type,
+                        category_url: item.url
+                    };
+                    cards.push(card_data);
+                });
             });
-        });
 
-        var html = $('<div class="trailers-list"></div>');
+            var html = $('<div class="trailers-list"></div>');
 
-        cards.forEach(function (card) {
-            var card_html = $('<div class="trailers-card selector"></div>');
-            var img = $('<div class="trailers-card__img"><img src="' + card.img + '" /></div>');
-            var title = $('<div class="trailers-card__title">' + card.title + '</div>');
-            var date_html = card.date ? $('<div class="trailers-card__date">' + card.date + '</div>') : $();
+            cards.forEach(function (card) {
+                var card_html = $('<div class="trailers-card selector"></div>');
+                var img = $('<div class="trailers-card__img"><img src="' + card.img + '" /></div>');
+                var title = $('<div class="trailers-card__title">' + card.title + '</div>');
+                var date_html = card.date ? $('<div class="trailers-card__date">' + card.date + '</div>') : $();
 
-            card_html.append(img).append(title);
-            if (card.date) {
-                card_html.append(date_html);
+                card_html.append(img).append(title);
+                if (card.date) {
+                    card_html.append(date_html);
+                }
+
+                card_html.data('card', card);
+
+                card_html.on('hover:enter', function () {
+                    var _card = $(this).data('card');
+                    Api.videos(_card, function (videos) {
+                        if (videos.results && videos.results.length) {
+                            var video = videos.results[0];
+                            var url = 'https://www.youtube.com/watch?v=' + video.key;
+                            Lampa.Player.play({
+                                url: url,
+                                title: _card.title,
+                                quality: { 1080: url },
+                                only_one_player: true
+                            });
+                            Lampa.Player.playlist([{
+                                url: url,
+                                title: _card.title
+                            }]);
+                        }
+                    }, function () {
+                        Lampa.Noty.show(Lampa.Lang.translate('trailers_no_trailer'));
+                    });
+                });
+
+                html.append(card_html);
+            });
+
+            if (params && params.append) {
+                params.append.append(html);
             }
 
-            card_html.data('card', card);
+            return cards.length ? html : false;
+        },
 
-            card_html.on('hover:enter', function () {
-                var _card = $(this).data('card');
-                Api.videos(_card, function (videos) {
-                    if (videos.results && videos.results.length) {
-                        var video = videos.results[0];
-                        var url = 'https://www.youtube.com/watch?v=' + video.key;
-                        Lampa.Player.play({
-                            url: url,
-                            title: _card.title,
-                            quality: { 1080: url },
-                            only_one_player: true
-                        });
-                        Lampa.Player.playlist([{
-                            url: url,
-                            title: _card.title
-                        }]);
-                    }
-                }, function () {
-                    Lampa.Noty.show(Lampa.Lang.translate('trailers_no_trailer'));
+        render: function (data, params) {
+            var html = $('<div></div>');
+
+            data.forEach(function (item) {
+                var title = $('<div class="trailers-category__title">' + item.title + '</div>');
+                var list = Component.render_cards([item], params);
+                var more = item.type && item.url ? $('<div class="trailers-category__more selector">' + Lampa.Lang.translate('trailers_more') + '</div>') : $();
+
+                more.on('hover:enter', function () {
+                    Lampa.Activity.push({
+                        url: item.url,
+                        title: item.title,
+                        component: 'trailers_full',
+                        type: item.type,
+                        page: 1
+                    });
                 });
+
+                var category = $('<div class="trailers-category"></div>');
+                category.append(title);
+                if (list) category.append(list);
+                category.append(more);
+
+                html.append(category);
             });
 
-            html.append(card_html);
-        });
+            return html;
+        },
 
-        if (params && params.append) {
-            params.append.append(html);
+        full: function (data, params) {
+            var html = Component.render_cards([data], { append: false });
+            if (html) {
+                var title = $('<div class="trailers-category__title">' + data.title + '</div>');
+                var category = $('<div class="trailers-category"></div>');
+                category.append(title).append(html);
+                return category;
+            }
+            return $('<div></div>');
+        },
+
+        clear: function () {
+            // Clear logic if needed
         }
+    };
 
-        return cards.length ? html : false;
-    },
+    console.log('Trailers', 'Component defined');
 
-    render: function (data, params) {
-        var html = $('<div></div>');
-
-        data.forEach(function (item) {
-            var title = $('<div class="trailers-category__title">' + item.title + '</div>');
-            var list = Component.render_cards([item], params);
-            var more = item.type && item.url ? $('<div class="trailers-category__more selector">' + Lampa.Lang.translate('trailers_more') + '</div>') : $();
-
-            more.on('hover:enter', function () {
-                Lampa.Activity.push({
-                    url: item.url,
-                    title: item.title,
-                    component: 'trailers_full',
-                    type: item.type,
-                    page: 1
-                });
-            });
-
-            var category = $('<div class="trailers-category"></div>');
-            category.append(title);
-            if (list) category.append(list);
-            category.append(more);
-
-            html.append(category);
-        });
-
-        return html;
-    },
-
-    full: function (data, params) {
-        var html = Component.render_cards([data], { append: false });
-        if (html) {
-            var title = $('<div class="trailers-category__title">' + data.title + '</div>');
-            var category = $('<div class="trailers-category"></div>');
-            category.append(title).append(html);
-            return category;
-        }
-        return $('<div></div>');
-    },
-
-    clear: function () {
-        // Clear logic if needed
-    }
-};
+    window.TrailersComponent = Component;
+})();
