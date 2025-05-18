e5ce6db910b364b5f60ecae58b7d2551f143ea9c
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-05-18 12:01:58 +0300

    Update plugin.js

--- src/plugin.js
+++ src/plugin.js
@@ -28,16 +28,31 @@
     });
 
     function startPlugin() {
-      if (window.plugin_trailers_ready) return;
+      if (window.plugin_trailers_ready) {
+        console.log('Plugin Trailers: Already initialized, skipping');
+        return;
+      }
       window.plugin_trailers_ready = true;
+      console.log('Plugin Trailers: Starting initialization');
+      console.log('Plugin Trailers: ComponentMain available:', !!TrailerPlugin.ComponentMain);
+      console.log('Plugin Trailers: ComponentFull available:', !!TrailerPlugin.ComponentFull);
+      if (!TrailerPlugin.ComponentMain || !TrailerPlugin.ComponentFull) {
+        console.error('Plugin Trailers: Missing components, delaying initialization');
+        setTimeout(startPlugin, 1000);
+        return;
+      }
       Lampa.Component.add('trailers_main', TrailerPlugin.ComponentMain);
       Lampa.Component.add('trailers_full', TrailerPlugin.ComponentFull);
+      console.log('Plugin Trailers: Components registered');
       Lampa.Template.add('trailer', '<div class="card selector card--trailer layer--render layer--visible"><div class="card__view"><img src="./img/img_load.svg" class="card__img"><div class="card__promo"><div class="card__promo-text"><div class="card__title"></div></div><div class="card__details"></div></div></div><div class="card__play"><img src="./img/icons/player/play.svg"></div></div>');
+      Lampa.Template.add('trailer_line', '<div class="items-line"><div class="items-line__title"><span>{{title}}</span></div><div class="items-line__body"><div class="trailer-line__cards"></div></div></div>');
       Lampa.Template.add('trailer_style', '<style>.card.card--trailer,.card-more.more--trailers{width:25.7em}.card.card--trailer .card__view{padding-bottom:56%;margin-bottom:0}.card.card--trailer .card__details{margin-top:0.8em}.card.card--trailer .card__play{position:absolute;top:50%;transform:translateY(-50%);left:1.5em;background:#000000b8;width:2.2em;height:2.2em;border-radius:100%;text-align:center;padding-top:0.6em}.card.card--trailer .card__play img{width:0.9em;height:1em}.card.card--trailer .card__rating{position:absolute;bottom:0.5em;right:0.5em;background:#000000b8;padding:0.2em 0.5em;border-radius:3px;font-size:1.2em}.card.card--trailer .card__trailer-lang{position:absolute;top:0.5em;right:0.5em;background:#000000b8;padding:0.2em 0.5em;border-radius:3px;text-transform:uppercase;font-size:1.2em}.card.card--trailer .card__release-date{position:absolute;top:2.5em;right:0.5em;background:#000000b8;padding:0.2em 0.5em;border-radius:3px;font-size:1.2em}.card-more.more--trailers .card-more__box{padding-bottom:56%}.category-full--trailers{display:flex;flex-wrap:wrap;justify-content:space-between}.category-full--trailers .card{width:33.3%;margin-bottom:1.5em}.category-full--trailers .card .card__view{padding-bottom:56%;margin-bottom:0}.items-line__more{display:inline-block;margin-left:10px;cursor:pointer;padding:0.5em 1em}@media screen and (max-width:767px){.category-full--trailers .card{width:50%}}@media screen and (max-width:400px){.category-full--trailers .card{width:100%}}</style>');
 
       function add() {
+        console.log('Plugin Trailers: Adding menu item');
         var button = $('<li class="menu__item selector"><div class="menu__ico"><svg height="70" viewBox="0 0 80 70" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M71.2555 2.08955C74.6975 3.2397 77.4083 6.62804 78.3283 10.9306C80 18.7291 80 35 80 35C80 35 80 51.2709 78.3283 59.0694C77.4083 63.372 74.6975 66.7603 71.2555 67.9104C65.0167 70 40 70 40 70C40 70 14.9833 70 8.74453 67.9104C5.3025 66.7603 2.59172 63.372 1.67172 59.0694C0 51.2709 0 35 0 35C0 35 0 18.7291 1.67172 10.9306C2.59172 6.62804 5.3025 3.2395 8.74453 2.08955C14.9833 0 40 0 40 0C40 0 65.0167 0 71.2555 2.08955ZM55.5909 35.0004L29.9773 49.5714V20.4286L55.5909 35.0004Z" fill="currentColor"/></svg></div><div class="menu__text">' + Lampa.Lang.translate('title_trailers') + '</div></li>');
         button.on('hover:enter', function() {
+          console.log('Plugin Trailers: Menu item clicked, pushing activity');
           Lampa.Activity.push({
             url: '',
             title: Lampa.Lang.translate('title_trailers'),
@@ -49,29 +64,36 @@
         $('body').append(Lampa.Template.get('trailer_style', {}, true));
         Lampa.Storage.listener.follow('change', function(event) {
           if (event.name === 'language') {
+            console.log('Plugin Trailers: Language changed, clearing cache');
             TrailerPlugin.Api.clear();
           }
         });
       }
 
       if (Lampa.TMDB && Lampa.TMDB.key()) {
+        console.log('Plugin Trailers: TMDB API key found, adding plugin');
         add();
       } else {
+        console.error('Plugin Trailers: TMDB API key missing');
         Lampa.Noty.show('TMDB API key is missing. Trailers plugin cannot be loaded.');
       }
     }
 
     if (!window.appready) {
+      console.log('Plugin Trailers: Waiting for app ready');
       Lampa.Listener.follow('app', function(e) {
         if (e.type === 'ready') {
+          console.log('Plugin Trailers: App ready, starting plugin');
           startPlugin();
         }
       });
     } else {
+      console.log('Plugin Trailers: App already ready, starting plugin');
       startPlugin();
     }
   }
 
+  console.log('Plugin Trailers: Script loaded');
   window.TrailerPlugin = window.TrailerPlugin || {};
   window.TrailerPlugin.init = initPlugin;
 })();
