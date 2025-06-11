5cb6e17484a1825dcfb1beed9dd86b5c6902027f
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-06-11 14:50:25 +0300

    Update t1.js

--- t1.js
+++ t1.js
@@ -191,10 +191,10 @@
     ];
 
     // Fallback data for testing
-    var fallbackCollections = collections.map(function (i, index) {
+    var fallbackCollections = collections.map(function (item, index) {
         return {
-            id: i.hpu + '_fallback',
-            title: i.title,
+            id: item.hpu + '_fallback_' + index,
+            title: item.title,
             backdrop_path: '',
             items_count: 0,
             time: new Date().toISOString(),
@@ -208,57 +208,69 @@
         var status = new Lampa.Status(collections.length);
 
         status.onComplite = function () {
-            let keys = Object.keys(status.data);
-            let sort = collections.map(a => a.hpu);
+            var keys = Object.keys(status.data);
+            var sort = collections.map(function (a) { return a.hpu; });
 
             if (keys.length) {
                 console.log('Collections loaded:', keys);
-                let fulldata = keys.map(key => {
-                    let data = status.data[key];
-                    data.title = collections.find(item => item.hpu === key).title;
-                    data.cardClass = Collection;
-                    return data;
-                }).sort((a, b) => sort.indexOf(a.category) - 'sort.indexOf(b.category.hpu));
-                onComplite(fulldata);
+                var fulldata = [];
+                keys.sort(function (a, b) {
+                    return sort.indexOf(a) - sort.indexOf(b);
+                });
+                keys.forEach(function (key) {
+                    var data = status.data[key];
+                    data.title = collections.find(function (item) {
+                        return item.hpu === key;
+                    }).title;
+                    data.cardClass = function (elem, param) {
+                        return new Collection(elem, param);
+                    };
+                    fulldata.push(data);
+                });
+                oncomplite(fulldata);
             } else {
                 console.warn('No collections loaded, using fallback data.');
                 Lampa.Noty.show(Lampa.Lang.translate('uaCollections_no_data'));
-                let fallback_data = [{
+                var fallback_data = [{
                     collection: true,
                     line_type: 'collection',
                     category: 'fallback',
                     title: Lampa.Lang.translate('uaCollections_title'),
                     results: fallbackCollections,
-                    cardClass: Collection
+                    cardClass: function (elem, param) {
+                        return new Collection(elem, param);
+                    }
                 }];
-                onComplite(fallback_data);
+                oncomplite(fallback_data);
             }
         };
 
         collections.forEach(function (item) {
-            let apiPath = item.type === 'collection' ? `collection/${item.url}` : item.url;
-            let url = Lampa.TMDB.api(apiPath);
+            var apiPath = item.type === 'collection' ? `collection/${item.url}` : item.url;
+            var url = Lampa.TMDB.api(apiPath);
             console.log('Requesting TMDB:', url);
             network.silent(url, function (data) {
                 console.log('TMDB response for', item.hpu, ':', data);
-                let results = item.type === 'collection' ? (data.parts || []) : (data.results || []).slice(0, 10);
+                var results = item.type === 'collection' ? (data.parts || []) : (data.results || []).slice(0, 10);
                 if (!results.length) {
                     console.warn('No results for', item.hpu);
                     Lampa.Noty.show(Lampa.Lang.translate('uaCollections_no_data') + ': ' + item.title);
                 }
-                let collection_data = {
+                var collection_data = {
                     collection: true,
                     line_type: 'collection',
                     category: item.hpu,
-                    results: results.map((result, index) => ({
-                        id: item.hpu + '_' + index,
-                        title: result.title || result.name || 'Untitled',
-                        backdrop_path: result.backdrop_path || result.poster_path,
-                        items_count: results.length,
-                        time: new Date().toISOString(),
-                        views: Math.floor(Math.random() * 10000),
-                        liked: Math.floor(Math.random() * 1000)
-                    }))
+                    results: results.map(function (result, index) {
+                        return {
+                            id: item.hpu + '_' + index,
+                            title: result.title || result.name || 'Untitled',
+                            backdrop_path: result.backdrop_path || result.poster_path,
+                            items_count: results.length,
+                            time: new Date().toISOString(),
+                            views: Math.floor(Math.random() * 10000),
+                            liked: Math.floor(Math.random() * 1000)
+                        };
+                    })
                 };
                 status.append(item.hpu, collection_data);
             }, function (error) {
@@ -266,24 +278,24 @@
                 Lampa.Noty.show(Lampa.Lang.translate('uaCollections_error').format(item.title));
                 status.error();
             });
-        })();
+        });
     }
 
     function collection(params, oncomplite, onerror) {
         console.log('Fetching collection:', params.url);
-        let category = collections.find(c => c.hpu === params.url);
+        var category = collections.find(function (c) { return c.hpu === params.url; });
         if (!category) {
             console.error('Category not found:', params.url);
             return onerror();
         }
 
-        let apiPath = category.type === 'collection' ? `collection/${category.url}` : category.url + '&page=' + params.page;
-        let url = Lampa.TMDB(apiPath);
-        console.log(url);
+        var apiPath = category.type === 'collection' ? `collection/${category.url}` : category.url + '&page=' + params.page;
+        var url = Lampa.TMDB.api(apiPath);
+        console.log('Requesting collection:', url);
         network.silent(url, function (data) {
             console.log('Collection response for', category.hpu, ':', data);
-            let results = category.type === 'collection' ? (data.parts || []) : (data.results || []);
-            let collection_data = {
+            var results = category.type === 'collection' ? (data.parts || []) : (data.results || []);
+            var collection_data = {
                 collection: true,
                 total_pages: category.type === 'collection' ? 1 : (data.total_pages || 15),
                 results: results.map(function (result, index) {
@@ -295,10 +307,11 @@
                         time: new Date().toISOString(),
                         views: Math.floor(Math.random() * 10000),
                         liked: Math.floor(Math.random() * 1000)
-                    },
-                );
-            },
-            cardClass: Collection
+                    };
+                }),
+                cardClass: function (elem, param) {
+                    return new Collection(elem, param);
+                }
             };
             oncomplite(collection_data);
         }, function (error) {
@@ -310,28 +323,30 @@
 
     function full(params, oncomplite, onerror) {
         console.log('Fetching full collection:', params.url);
-        let category = collections.find(c => c.hpu === params.url.split('_')[0]);
+        var category = collections.find(function (c) { return c.hpu === params.url.split('_')[0]; });
         if (!category) {
             console.error('Category not found:', params.url);
             return onerror();
         }
 
-        let apiPath = category.type === 'collection' ? `collection/${category.url}` : category.url + '&page=' + params.page;
-        let url = Lampa.TMDB.api(apiPath);
+        var apiPath = category.type === 'collection' ? `collection/${category.url}` : category.url + '&page=' + params.page;
+        var url = Lampa.TMDB.api(apiPath);
         console.log('Requesting full:', url);
         network.silent(url, function (data) {
             console.log('Full response for', category.hpu, ':', data);
-            let results = category.type === 'collection' ? (data.parts || []) : (data.results || []);
-            let collection_data = {
+            var results = category.type === 'collection' ? (data.parts || []) : (data.results || []);
+            var collection_data = {
                 total_pages: category.type === 'collection' ? 1 : (data.total_pages || 15),
-                results: results.map(result => ({
-                    id: result.id,
-                    title: result.title || result.name || 'Untitled',
-                    poster_path: result.poster_path,
-                    backdrop_path: result.backdrop_path,
-                    overview: result.overview,
-                    release_date: result.release_date || result.first_air_date
-                }))
+                results: results.map(function (result) {
+                    return {
+                        id: result.id,
+                        title: result.title || result.name || 'Untitled',
+                        poster_path: result.poster_path,
+                        backdrop_path: result.backdrop_path,
+                        overview: result.overview,
+                        release_date: result.release_date || result.first_air_date
+                    };
+                })
             };
             oncomplite(collection_data);
         }, function (error) {
@@ -457,87 +472,23 @@
         console.log('Starting UACollections plugin');
         var manifest = {
             type: 'video',
-            version: '1.0.3',
+            version: '1.0.4',
             name: Lampa.Lang.translate('uaCollections_title'),
             description: 'Themed movie and series collections powered by TMDB',
             component: 'ua_collections'
         };
         Lampa.Manifest.plugins = manifest;
 
-        Lampa.Component.add('ua_collections_main', componentMain);
-        Lampa.Component.add('ua_collections_collection', componentCollection);
-        Lampa.Component.add('ua_collections_view', componentView);
+        console.log('Adding template: ua_collection');
+        Lampa.Template.add('ua_collection', `<div class="card ua-collection-card selector layer--visible layer--render card--collection"><div class="card__view"><img src="./img/img_load.svg" class="card__img"><div class="ua-collection-card__head"><div class="ua-collection-card__items"></div><div class="ua-collection-card__date"></div></div><div class="ua-collection-card__bottom"><div class="ua-collection-card__views"></div></div></div><div class="card__title"></div></div>`);
 
-        Lampa.Template.add('ua_collection', `
-            <div class="card ua-collection-card selector layer--visible layer--render card--collection">
-                <div class="card__view">
-                    <img src="./img/img_load.svg" class="card__img">
-                    <div class="ua-collection-card__head">
-                        <div class="ua-collection-card__items"></div>
-                        <div class="ua-collection-card__date"></div>
-                    </div>
-                    <div class="ua-collection-card__bottom">
-                        <div class="ua-collection-card__views"></div>
-                    </div>
-                </div>
-                <div class="card__title"></div>
-            </div>
-        `);
-
-        var style = `
-            <style>
-            .ua-collection-card__head {
-                display: flex;
-                align-items: center;
-                justify-content: space-between;
-                padding: .5em 1em;
-                color: #fff;
-                font-size: 1em;
-                font-weight: 500;
-                position: absolute;
-                top: 0;
-                left: 0;
-                width: 100%;
-            }
-            .ua-collection-card__bottom {
-                display: flex;
-                align-items: center;
-                padding: .5em 1em;
-                background-color: rgba(0,0,0,0.5);
-                color: #fff;
-                font-size: 1em;
-                font-weight: 400;
-                border-radius: 1em;
-                position: absolute;
-                bottom: 0;
-                left: 0;
-                width: 100%;
-            }
-            .ua-collection-card__items {
-                background: rgba(0,0,0,0.5);
-                padding: .3em;
-                border-radius: .2em;
-            }
-            .category-full .ua-collection-card {
-                padding-bottom: 2em;
-            }
-            </style>
-        `;
-        Lampa.Template.add('ua_collections_css', style);
+        console.log('Adding template: ua_collections_css');
+        Lampa.Template.add('ua_collections_css', `<style>.ua-collection-card__head{display:flex;align-items:center;justify-content:space-between;padding:.5em 1em;color:#fff;font-size:1em;font-weight:500;position:absolute;top:0;left:0;width:100%}.ua-collection-card__bottom{display:flex;align-items:center;padding:.5em 1em;background-color:rgba(0,0,0,0.5);color:#fff;font-size:1em;font-weight:400;border-radius:1em;position:absolute;bottom:0;left:0;width:100%}.ua-collection-card__items{background:rgba(0,0,0,0.5);padding:.3em;border-radius:.2em}.category-full .ua-collection-card{padding-bottom:2em}</style>`);
         $('body').append(Lampa.Template.get('ua_collections_css', {}, true));
 
         function add() {
             console.log('Adding menu button');
-            var button = $(`
-                <li class="menu__item selector">
-                    <div class="menu__ico">
-                        <svg width="191" height="239" viewBox="0 0 191 239" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M35.3438 35.3414V26.7477C35.3438 19.9156 38.0594 13.3543 42.8934 8.51604C47.7297 3.68251 54.2874 0.967027 61.125 0.966431H164.25C171.086 0.966431 177.643 3.68206 182.482 8.51604C187.315 13.3524 190.031 19.91 190.031 26.7477V186.471C190.031 189.87 189.022 193.192 187.133 196.018C185.245 198.844 182.561 201.046 179.421 202.347C176.28 203.647 172.825 203.988 169.492 203.325C166.158 202.662 163.096 201.026 160.692 198.623L155.656 193.587V220.846C155.656 224.245 154.647 227.567 152.758 230.393C150.87 233.219 148.186 235.421 145.046 236.722C141.905 238.022 138.45 238.363 135.117 237.7C131.783 237.037 128.721 235.401 126.317 232.998L78.3125 184.993L30.3078 232.998C27.9041 235.401 24.8419 237.037 21.5084 237.7C18.1748 238.363 14.7195 238.022 11.5794 236.722C8.43922 235.421 5.75517 233.219 3.86654 230.393C1.9779 227.567 0.969476 224.245 0.96875 220.846V61.1227C0.96875 54.2906 3.68437 47.7293 8.51836 42.891C13.3547 38.0575 19.9124 35.342 26.75 35.3414H35.3438ZM138.469 220.846V61.1227C138.469 58.8435 137.563 56.6576 135.952 55.046C134.34 53.4343 132.154 52.5289 129.875 52.5289H26.75C24.4708 52.5289 22.2849 53.4343 20.6733 55.046C19.0617 56.6576 18.1562 58.8435 18.1562 61.1227V220.846L66.1609 172.841C69.3841 169.619 73.755 167.809 78.3125 167.809C82.87 167.809 87.2409 169.619 90.4641 172.841L138.469 220.846ZM155.656 169.284L172.844 186.471V26.7477C172.844 24.4685 171.938 22.2826 170.327 20.671C168.715 19.0593 166.529 18.1539 164.25 18.1539H61.125C58.8458 18.1539 56.6599 19.0593 55.0483 20.671C53.4367 22.2826 52.5312 24.4685 52.5312 26.7477V35.3414H129.875C136.711 35.3414 143.268 38.0571 148.107 42.891C152.94 47.7274 155.656 54.285 155.656 61.1227V169.284Z" fill="currentColor"/>
-                        </svg>
-                    </div>
-                    <div class="menu__text">${manifest.name}</div>
-                </li>
-            `);
+            var button = $(`<li class="menu__item selector"><div class="menu__ico"><svg width="191" height="239" viewBox="0 0 191 239" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M35.3438 35.3414V26.7477C35.3438 19.9156 38.0594 13.3543 42.8934 8.51604C47.7297 3.68251 54.2874 0.967027 61.125 0.966431H164.25C171.086 0.966431 177.643 3.68206 182.482 8.51604C187.315 13.3524 190.031 19.91 190.031 26.7477V186.471C190.031 189.87 189.022 193.192 187.133 196.018C185.245 198.844 182.561 201.046 179.421 202.347C176.28 203.647 172.825 203.988 169.492 203.325C166.158 202.662 163.096 201.026 160.692 198.623L155.656 193.587V220.846C155.656 224.245 154.647 227.567 152.758 230.393C150.87 233.219 148.186 235.421 145.046 236.722C141.905 238.022 138.45 238.363 135.117 237.7C131.783 237.037 128.721 235.401 126.317 232.998L78.3125 184.993L30.3078 232.998C27.9041 235.401 24.8419 237.037 21.5084 237.7C18.1748 238.363 14.7195 238.022 11.5794 236.722C8.43922 235.421 5.75517 233.219 3.86654 230.393C1.9779 227.567 0.969476 224.245 0.96875 220.846V61.1227C0.96875 54.2906 3.68437 47.7293 8.51836 42.891C13.3547 38.0575 19.9124 35.342 26.75 35.3414H35.3438ZM138.469 220.846V61.1227C138.469 58.8435 137.563 56.6576 135.952 55.046C134.34 53.4343 132.154 52.5289 129.875 52.5289H26.75C24.4708 52.5289 22.2849 53.4343 20.6733 55.046C19.0617 56.6576 18.1562 58.8435 18.1562 61.1227V220.846L66.1609 172.841C69.3841 169.619 73.755 167.809 78.3125 167.809C82.87 167.809 87.2409 169.619 90.4641 172.841L138.469 220.846ZM155.656 169.284L172.844 186.471V26.7477C172.844 24.4685 171.938 22.2826 170.327 20.671C168.715 19.0593 166.529 18.1539 164.25 18.1539H61.125C58.8458 18.1539 56.6599 19.0593 55.0483 20.671C53.4367 22.2826 52.5312 24.4685 52.5312 26.7477V35.3414H129.875C136.711 35.3414 143.268 38.0571 148.107 42.891C152.94 47.7274 155.656 54.285 155.656 61.1227V169.284Z" fill="currentColor"/></svg></div><div class="menu__text">${manifest.name}</div></li>`);
             button.on('hover:enter', function () {
                 console.log('Menu button clicked');
                 Lampa.Activity.push({
