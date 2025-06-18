82240b6109176223d154e205b1498e58c912d3f2
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-06-18 17:37:31 +0300

    Update person.js

--- person.js
+++ person.js
@@ -1,41 +1,41 @@
-            // Для страницы актера
-            if (e.type === 'start' && e.component === 'actor') {
-                log("[PERSON-PLUGIN] Actor page started");
-                
-                if (e.object && e.object.id) {
-                    currentPersonId = parseInt(e.object.id, 10);
-                    log("[PERSON-PLUGIN] Found actor ID in e.object.id:", currentPersonId);
-                    
-                    // Используем улучшенное ожидание контейнера
-                    waitForContainer(function() {
-                        addSubscribeButton();
-                    });
-                }
-            }
-            // При активации страницы плагина
-            else if (e.type === 'resume' && e.component === 'category_full' && e.object && e.object.source === PLUGIN_NAME) {
-                log("[PERSON-PLUGIN] Persons list resumed");
-                // Обновляем список при возврате
-                setTimeout(function() {
-                    Lampa.Activity.reload();
-                }, 100);
-        });
-}
-        // Запускаем проверку текущей активности
-        setTimeout(checkCurrentActivity, 1500);
-        
-        // Добавляем стили
-        addButtonStyles();
-    }
+// Для страницы актера
+if (e.type === 'start' && e.component === 'actor') {
+    log("[PERSON-PLUGIN] Actor page started");
 
-    // Запуск плагина
-    if (window.appready) {
-        startPlugin();
-    } else {
-        Lampa.Listener.follow('app', function(e) {
-            if (e.type === 'ready') {
-                startPlugin();
-            }
+    if (e.object && e.object.id) {
+        currentPersonId = parseInt(e.object.id, 10);
+        log("[PERSON-PLUGIN] Found actor ID in e.object.id:", currentPersonId);
+
+        // Используем улучшенное ожидание контейнера
+        waitForContainer(function() {
+            addSubscribeButton();
         });
     }
+}
+// При активации страницы плагина
+else if (e.type === 'resume' && e.component === 'category_full' && e.object && e.object.source === PLUGIN_NAME) {
+    log("[PERSON-PLUGIN] Persons list resumed");
+    // Обновляем список при возврате
+    setTimeout(function() {
+        Lampa.Activity.reload();
+    }, 100);
+} // ← Цієї дужки не вистачало в оригіналі!
+
+// Запускаем проверку текущей активности
+setTimeout(checkCurrentActivity, 1500);
+
+// Добавляем стили
+addButtonStyles();
+}
+
+// Запуск плагина
+if (window.appready) {
+    startPlugin();
+} else {
+    Lampa.Listener.follow('app', function(e) {
+        if (e.type === 'ready') {
+            startPlugin();
+        }
+    });
+}
 }();
