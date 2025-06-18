f87491658990dad0aa7e9fd88b88c5ecd4f8f693
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-06-18 18:00:13 +0300

    Update t2.js

--- t2.js
+++ t2.js
@@ -2,9 +2,9 @@
     'use strict';
 
     // Перевірка, чи плагін уже ініціалізовано
-    if (window.SeasonSeriaPlugin && window.SeasonSeriaPlugin.__initialized) return;
+    if (window.SeasonSeriaWindow && window.SeasonSeriaPlugin.__initialized) return;
     window.SeasonSeriaPlugin = window.SeasonSeriaPlugin || {};
-    window.SeasonSeriaPlugin.__initialized = true;
+    window.SeasonSeriaPlugin.___initialized = true;
 
     // Додавання локалізації для англійської та української мов
     Lampa.Lang.add({
@@ -17,20 +17,20 @@
             uk: "Сезон {season}\nЕпізодів {current}/{total}"
         },
         season_seria_season_completed: {
-            en: "Season {season}\nCompleted",
-            uk: "Сезон {season}\nЗавершено"
+            en: "Season {season}\nEpisodes {episodes}",
+            uk: "Сезон {season}\nЕпізодів {episodes}"
         },
         season_seria_series_ended: {
             en: "Seasons {seasons} Episodes {episodes}\nEnded",
-            uk: "Сезонів {seasons} Епізодів {episodes}\nЗакінчено"
+            uk: "Сезонів {seasons} Епізодів {episodes}\nЗакінчено}"
         },
         season_seria_series_canceled: {
             en: "Seasons {seasons} Episodes {episodes}\nCanceled",
-            uk: "Сезонів {seasons} Епізодів {episodes}\nПрипинено"
+            uk: "Сезонів {seasons} Епізодів {episodes}\nПрипинено}"
         },
         season_seria_series_planned: {
             en: "Season {season}\nPlanned",
-            uk: "Сезон {season}\nЗаплановано"
+            uk: "Сезон {season}\nЗаплановано}"
         }
     });
 
@@ -62,7 +62,7 @@
     function initPlugin() {
         if (!isSeasonSeriaEnabled()) return;
 
-        // Додаємо CSS для батьківського контейнера та стилів тегів
+        // Додаємо CSS для батьківського контейнера та стилів тегу
         var style = $('<style>' +
             '.full-start__poster, .full-start-new__poster { position: relative; width: 100%; }' +
             '.card--new_seria { ' +
@@ -89,26 +89,6 @@
             'display: block; ' +
             'white-space: pre; ' +
             '}' +
-            '.card__vote { ' +
-            'position: absolute; ' +
-            'right: 0.3em; ' +
-            'bottom: 0.3em; ' +
-            'background: rgba(0, 0, 0, 0.5); ' +
-            'color: #fff; ' +
-            'font-size: 1.1em; ' +
-            'font-weight: 700; ' +
-            'padding: 0.2em 0.5em; ' +
-            'border-radius: 1em; ' +
-            'z-index: 10; ' +
-            'display: inline-block; ' +
-            'vertical-align: middle; ' +
-            'min-height: 2.4em; ' +
-            'line-height: 1.2; ' +
-            '-webkit-user-select: none; ' +
-            '-moz-user-select: none; ' +
-            '-ms-user-select: none; ' +
-            'user-select: none; ' +
-            '}' +
             '</style>');
         $('head').append(style);
 
@@ -117,20 +97,13 @@
             if (event.type === 'complite' && Lampa.Activity.active().component === 'full') {
                 var data = Lampa.Activity.active().card;
 
-                // Перевірка, чи це серіал або фільм із джерела tmdb
-                if (data && data.source === 'tmdb') {
+                // Перевірка, чи це серіал із джерела tmdb
+                if (data && data.source === 'tmdb' && data.seasons && isSeasonSeriaEnabled()) {
                     var activityRender = Lampa.Activity.active().activity.render();
                     var cardContainer = $('.full-start__poster, .full-start-new__poster', activityRender);
 
-                    // Додавання тегу оцінки (.card__vote), якщо є vote_average
-                    if (data.vote_average && !$('.card__vote', activityRender).length && cardContainer.length) {
-                        var voteText = data.vote_average.toFixed(1);
-                        var voteTag = '<div class="card__vote">' + voteText + '</div>';
-                        cardContainer.append(voteTag);
-                    }
-
-                    // Додавання тегу сезону та серії (.card--new_seria), якщо це серіал
-                    if (data.seasons && isSeasonSeriaEnabled() && !$('.card--new_seria', activityRender).length && cardContainer.length) {
+                    // Додавання тегу сезону та серії (.card--new_seria)
+                    if (!$('.card--new_seria', activityRender).length && cardContainer.length) {
                         var seasonNumber = data.last_episode_to_air ? data.last_episode_to_air.season_number : 1;
                         var episodeNumber = data.last_episode_to_air ? data.last_episode_to_air.episode_number : 0;
                         var nextEpisode = data.next_episode_to_air;
@@ -167,7 +140,8 @@
                         } else if (!nextEpisode && data.last_episode_to_air) {
                             // Випадок 2: Сезон завершено
                             labelText = Lampa.Lang.translate('season_seria_season_completed')
-                                .replace('{season}', seasonNumber);
+                                .replace('{season}', seasonNumber)
+                                .replace('{episodes}', episodeCount);
                         } else {
                             // Випадок 1: Активний сезон (Returning Series, In Production, Pilot)
                             labelText = Lampa.Lang.translate('season_seria_active')
