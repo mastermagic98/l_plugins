1054246c7624cd2186fcad6e0db4e6db7138f82f
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-06-24 10:29:54 +0300

    Update t3.js

--- t3.js
+++ t3.js
@@ -58,7 +58,7 @@
         if (!isSeasonSeriaEnabled()) return;
 
         var css = [
-            '.full-start__poster, .full-start-new__poster { position: relative; width: 100%; min-height: 200px; overflow: visible; }',
+            '.full-start { position: relative; width: 100%; min-height: 200px; overflow: visible; }',
             '.card--new_seria { position: relative; width: 100% !important; margin-top: 0.3em; background: red; color: #fff; font-size: 1.1em; font-weight: 700; padding: 0.2em 0.5em; border-radius: 1em; z-index: 20; display: block !important; visibility: visible !important; text-align: center; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; min-height: 2.4em; line-height: 1.2; }',
             '.card--new_seria span { display: block; white-space: pre; }'
         ].join('');
@@ -73,7 +73,8 @@
             if (!data || !data.seasons || !isSeasonSeriaEnabled()) return;
 
             var activityRender = Lampa.Activity.active().activity.render();
-            var cardContainer = $('.full-start__poster, .full-start-new__poster', activityRender);
+            var cardContainer = $('.full-start', activityRender);
+            console.log('cardContainer:', cardContainer.length, cardContainer);
             if ($('.card--new_seria', activityRender).length || !cardContainer.length) return;
 
             var seasonNumber = data.last_episode_to_air ? data.last_episode_to_air.season_number : 1;
