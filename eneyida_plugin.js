36fbb347d9c747aeea712f9ffef18354551c5f0a
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-07-03 16:54:50 +0300

    Update eneyida_plugin.js

--- eneyida_plugin.js
+++ eneyida_plugin.js
@@ -446,135 +446,148 @@
     }
 
     function startPlugin() {
-        window.eneyida_plugin = true;
-        var manifest = {
-            type: 'video',
-            version: '1.0.0',
-            name: 'Eneyida',
-            description: 'Плагін для перегляду фільмів і серіалів на eneyida.tv',
-            component: 'eneyida',
-            onContextMenu: function() {
-                return {
-                    name: Lampa.Lang.translate('lampac_watch'),
-                    description: 'Плагін для перегляду фільмів і серіалів на eneyida.tv'
-                };
-            },
-            onContextLauch: function(object) {
-                Lampa.Component.add('eneyida', component);
-                Lampa.Activity.push({
-                    url: '',
-                    title: Lampa.Lang.translate('title_online'),
+        // Перевірка, чи Lampa.Plugin доступний
+        function tryRegister(attempts) {
+            if (typeof Lampa.Plugin !== 'undefined') {
+                window.eneyida_plugin = true;
+                var manifest = {
+                    type: 'video',
+                    version: '1.0.0',
+                    name: 'Eneyida',
+                    description: 'Плагін для перегляду фільмів і серіалів на eneyida.tv',
                     component: 'eneyida',
-                    search: object.title,
-                    movie: object,
-                    page: 1
+                    onContextMenu: function() {
+                        return {
+                            name: Lampa.Lang.translate('lampac_watch'),
+                            description: 'Плагін для перегляду фільмів і серіалів на eneyida.tv'
+                        };
+                    },
+                    onContextLauch: function(object) {
+                        Lampa.Component.add('eneyida', component);
+                        Lampa.Activity.push({
+                            url: '',
+                            title: Lampa.Lang.translate('title_online'),
+                            component: 'eneyida',
+                            search: object.title,
+                            movie: object,
+                            page: 1
+                        });
+                    }
+                };
+
+                Lampa.Manifest.plugins = manifest;
+                Lampa.Lang.add({
+                    lampac_watch: {
+                        ru: 'Смотреть онлайн',
+                        uk: 'Дивитися онлайн',
+                        en: 'Watch online'
+                    },
+                    lampac_balanser: {
+                        ru: 'Источник',
+                        uk: 'Джерело',
+                        en: 'Source'
+                    },
+                    lampac_nolink: {
+                        ru: 'Не удалось извлечь ссылку',
+                        uk: 'Неможливо отримати посилання',
+                        en: 'Failed to fetch link'
+                    },
+                    empty_title_two: {
+                        ru: 'Ничего не найдено',
+                        uk: 'Нічого не знайдено',
+                        en: 'Nothing found'
+                    },
+                    empty_text: {
+                        ru: 'Попробуйте изменить запрос или выберите другой источник',
+                        uk: 'Спробуйте змінити запит або виберіть інше джерело',
+                        en: 'Try changing the query or select another source'
+                    },
+                    lampac_balanser_timeout: {
+                        ru: 'Источник будет переключен автоматически через <span class="timeout">5</span> секунд.',
+                        uk: 'Джерело буде автоматично переключено через <span class="timeout">5</span> секунд.',
+                        en: 'The source will be switched automatically after <span class="timeout">5</span> seconds.'
+                    }
                 });
-            }
-        };
 
-        Lampa.Manifest.plugins = manifest;
-        Lampa.Lang.add({
-            lampac_watch: {
-                ru: 'Смотреть онлайн',
-                uk: 'Дивитися онлайн',
-                en: 'Watch online'
-            },
-            lampac_balanser: {
-                ru: 'Источник',
-                uk: 'Джерело',
-                en: 'Source'
-            },
-            lampac_nolink: {
-                ru: 'Не удалось извлечь ссылку',
-                uk: 'Неможливо отримати посилання',
-                en: 'Failed to fetch link'
-            },
-            empty_title_two: {
-                ru: 'Ничего не найдено',
-                uk: 'Нічого не знайдено',
-                en: 'Nothing found'
-            },
-            empty_text: {
-                ru: 'Попробуйте изменить запрос или выберите другой источник',
-                uk: 'Спробуйте змінити запит або виберіть інше джерело',
-                en: 'Try changing the query or select another source'
-            },
-            lampac_balanser_timeout: {
-                ru: 'Источник будет переключен автоматически через <span class="timeout">5</span> секунд.',
-                uk: 'Джерело буде автоматично переключено через <span class="timeout">5</span> секунд.',
-                en: 'The source will be switched automatically after <span class="timeout">5</span> seconds.'
-            }
-        });
+                Lampa.Template.add('lampac_css', `
+                    <style>
+                        .online-prestige{position:relative;-webkit-border-radius:.3em;border-radius:.3em;background-color:rgba(0,0,0,0.3);display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex}
+                        .online-prestige__body{padding:1.2em;line-height:1.3;-webkit-box-flex:1;-webkit-flex-grow:1;-moz-box-flex:1;-ms-flex-positive:1;flex-grow:1;position:relative}
+                        @media screen and (max-width:480px){.online-prestige__body{padding:.8em 1.2em}}
+                        .online-prestige__img{position:relative;width:13em;-webkit-flex-shrink:0;-ms-flex-negative:0;flex-shrink:0;min-height:8.2em}
+                        .online-prestige__img>img{position:absolute;top:0;left:0;width:100%;height:100%;-o-object-fit:cover;object-fit:cover;-webkit-border-radius:.3em;border-radius:.3em;opacity:0;-webkit-transition:opacity .3s;-o-transition:opacity .3s;-moz-transition:opacity .3s;transition:opacity .3s}
+                        .online-prestige__img--loaded>img{opacity:1}
+                        @media screen and (max-width:480px){.online-prestige__img{width:7em;min-height:6em}}
+                        .online-prestige__loader{position:absolute;top:50%;left:50%;width:2em;height:2em;margin-left:-1em;margin-top:-1em;background:url(./img/loader.svg) no-repeat center center;-webkit-background-size:contain;-o-background-size:contain;background-size:contain}
+                        .online-prestige__head,.online-prestige__footer{display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-box-pack:justify;-webkit-justify-content:space-between;-moz-box-pack:justify;-ms-flex-pack:justify;justify-content:space-between;-webkit-box-align:center;-webkit-align-items:center;-moz-box-align:center;-ms-flex-align:center;align-items:center}
+                        .online-prestige__title{font-size:1.7em;overflow:hidden;-o-text-overflow:ellipsis;text-overflow:ellipsis;display:-webkit-box;-webkit-line-clamp:1;line-clamp:1;-webkit-box-orient:vertical}
+                        @media screen and (max-width:480px){.online-prestige__title{font-size:1.4em}}
+                        .online-prestige__time{padding-left:2em}
+                        .online-prestige__info{display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-box-align:center;-webkit-align-items:center;-moz-box-align:center;-ms-flex-align:center;align-items:center}
+                        .online-prestige__info>*{overflow:hidden;-o-text-overflow:ellipsis;text-overflow:ellipsis;display:-webkit-box;-webkit-line-clamp:1;line-clamp:1;-webkit-box-orient:vertical}
+                        .online-prestige__quality{padding-left:1em;white-space:nowrap}
+                        .online-prestige+.online-prestige{margin-top:1.5em}
+                        .online-empty__title{font-size:1.8em;margin-bottom:.3em}
+                        .online-empty__time{font-size:1.2em;font-weight:300;margin-bottom:1.6em}
+                        .online-empty__buttons{display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex}
+                        .online-empty__buttons>*+*{margin-left:1em}
+                        .online-empty__button{background:rgba(0,0,0,0.3);font-size:1.2em;padding:.5em 1.2em;-webkit-border-radius:.2em;border-radius:.2em;margin-bottom:2.4em}
+                        .online-empty__button.focus{background:#fff;color:black}
+                    </style>
+                `);
+                $('body').append(Lampa.Template.get('lampac_css', {}, true));
 
-        Lampa.Template.add('lampac_css', `
-            <style>
-                /* Додаємо стилі з smotrolet_plugin */
-                .online-prestige{position:relative;-webkit-border-radius:.3em;border-radius:.3em;background-color:rgba(0,0,0,0.3);display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex}
-                .online-prestige__body{padding:1.2em;line-height:1.3;-webkit-box-flex:1;-webkit-flex-grow:1;-moz-box-flex:1;-ms-flex-positive:1;flex-grow:1;position:relative}
-                @media screen and (max-width:480px){.online-prestige__body{padding:.8em 1.2em}}
-                .online-prestige__img{position:relative;width:13em;-webkit-flex-shrink:0;-ms-flex-negative:0;flex-shrink:0;min-height:8.2em}
-                .online-prestige__img>img{position:absolute;top:0;left:0;width:100%;height:100%;-o-object-fit:cover;object-fit:cover;-webkit-border-radius:.3em;border-radius:.3em;opacity:0;-webkit-transition:opacity .3s;-o-transition:opacity .3s;-moz-transition:opacity .3s;transition:opacity .3s}
-                .online-prestige__img--loaded>img{opacity:1}
-                @media screen and (max-width:480px){.online-prestige__img{width:7em;min-height:6em}}
-                .online-prestige__loader{position:absolute;top:50%;left:50%;width:2em;height:2em;margin-left:-1em;margin-top:-1em;background:url(./img/loader.svg) no-repeat center center;-webkit-background-size:contain;-o-background-size:contain;background-size:contain}
-                .online-prestige__head,.online-prestige__footer{display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-box-pack:justify;-webkit-justify-content:space-between;-moz-box-pack:justify;-ms-flex-pack:justify;justify-content:space-between;-webkit-box-align:center;-webkit-align-items:center;-moz-box-align:center;-ms-flex-align:center;align-items:center}
-                .online-prestige__title{font-size:1.7em;overflow:hidden;-o-text-overflow:ellipsis;text-overflow:ellipsis;display:-webkit-box;-webkit-line-clamp:1;line-clamp:1;-webkit-box-orient:vertical}
-                @media screen and (max-width:480px){.online-prestige__title{font-size:1.4em}}
-                .online-prestige__time{padding-left:2em}
-                .online-prestige__info{display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-box-align:center;-webkit-align-items:center;-moz-box-align:center;-ms-flex-align:center;align-items:center}
-                .online-prestige__info>*{overflow:hidden;-o-text-overflow:ellipsis;text-overflow:ellipsis;display:-webkit-box;-webkit-line-clamp:1;line-clamp:1;-webkit-box-orient:vertical}
-                .online-prestige__quality{padding-left:1em;white-space:nowrap}
-                .online-prestige+.online-prestige{margin-top:1.5em}
-                .online-empty__title{font-size:1.8em;margin-bottom:.3em}
-                .online-empty__time{font-size:1.2em;font-weight:300;margin-bottom:1.6em}
-                .online-empty__buttons{display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex}
-                .online-empty__buttons>*+*{margin-left:1em}
-                .online-empty__button{background:rgba(0,0,0,0.3);font-size:1.2em;padding:.5em 1.2em;-webkit-border-radius:.2em;border-radius:.2em;margin-bottom:2.4em}
-                .online-empty__button.focus{background:#fff;color:black}
-            </style>
-        `);
-        $('body').append(Lampa.Template.get('lampac_css', {}, true));
+                Lampa.Template.add('lampac_does_not_answer', `
+                    <div class="online-empty">
+                        <div class="online-empty__title">#{lampac_balanser_dont_work}</div>
+                        <div class="online-empty__time">#{lampac_balanser_timeout}</div>
+                        <div class="online-empty__buttons">
+                            <div class="online-empty__button selector cancel">#{cancel}</div>
+                            <div class="online-empty__button selector change">#{lampac_change_balanser}</div>
+                        </div>
+                    </div>
+                `);
 
-        Lampa.Template.add('lampac_does_not_answer', `
-            <div class="online-empty">
-                <div class="online-empty__title">#{lampac_balanser_dont_work}</div>
-                <div class="online-empty__time">#{lampac_balanser_timeout}</div>
-                <div class="online-empty__buttons">
-                    <div class="online-empty__button selector cancel">#{cancel}</div>
-                    <div class="online-empty__button selector change">#{lampac_change_balanser}</div>
-                </div>
-            </div>
-        `);
+                var button = `
+                    <div class="full-start__button selector view--online_eneyida eneyida--button" data-subtitle="${manifest.name} ${manifest.version}">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M16.088 6.412a2.84 2.84 0 0 0-1.347-.955l-1.378-.448a.544.544 0 0 1 0-1.025l1.378-.448A2.84 2.84 0 0 0 16.5 1.774l.011-.034l.448-1.377a.544.544 0 0 1 1.027 0l.447 1.377a2.84 2.84 0 0 0 1.799 1.796l1.377.448l.028.007a.544.544 0 0 1 0 1.025l-1.378.448a2.84 2.84 0 0 0-1.798 1.796l-.448 1.377l-.013.034a.544.544 0 0 1-1.013-.034l-.448-1.377a2.8 2.8 0 0 0-.45-.848m7.695 3.801l-.766-.248a1.58 1.58 0 0 1-.998-.999l-.25-.764a.302.302 0 0 0-.57 0l-.248.764a1.58 1.58 0 0 1-.984.999l-.765.248a.302.302 0 0 0 0 .57l.765.249a1.58 1.58 0 0 1 1 1.002l.248.764a.302.302 0 0 0 .57 0l.249-.764a1.58 1.58 0 0 1 .999-.999l.765-.248a.302.302 0 0 0 0-.57zM12 2c.957 0 1.883.135 2.76.386q-.175.107-.37.173l-1.34.44c-.287.103-.532.28-.713.508a8.5 8.5 0 1 0 8.045 9.909c.22.366.542.633 1.078.633q.185 0 .338-.04C20.868 18.57 16.835 22 12 22C6.477 22 2 17.523 2 12S6.477 2 12 2m-1.144 6.155A1.25 1.25 0 0 0 9 9.248v5.504a1.25 1.25 0 0 0 1.856 1.093l5.757-3.189a.75.75 0 0 0 0-1.312z"/>
+                        </svg>
+                        <span>#{title_online}</span>
+                    </div>
+                `;
 
-        var button = `
-            <div class="full-start__button selector view--online_eneyida eneyida--button" data-subtitle="${manifest.name} ${manifest.version}">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path fill="currentColor" d="M16.088 6.412a2.84 2.84 0 0 0-1.347-.955l-1.378-.448a.544.544 0 0 1 0-1.025l1.378-.448A2.84 2.84 0 0 0 16.5 1.774l.011-.034l.448-1.377a.544.544 0 0 1 1.027 0l.447 1.377a2.84 2.84 0 0 0 1.799 1.796l1.377.448l.028.007a.544.544 0 0 1 0 1.025l-1.378.448a2.84 2.84 0 0 0-1.798 1.796l-.448 1.377l-.013.034a.544.544 0 0 1-1.013-.034l-.448-1.377a2.8 2.8 0 0 0-.45-.848m7.695 3.801l-.766-.248a1.58 1.58 0 0 1-.998-.999l-.25-.764a.302.302 0 0 0-.57 0l-.248.764a1.58 1.58 0 0 1-.984.999l-.765.248a.302.302 0 0 0 0 .57l.765.249a1.58 1.58 0 0 1 1 1.002l.248.764a.302.302 0 0 0 .57 0l.249-.764a1.58 1.58 0 0 1 .999-.999l.765-.248a.302.302 0 0 0 0-.57zM12 2c.957 0 1.883.135 2.76.386q-.175.107-.37.173l-1.34.44c-.287.103-.532.28-.713.508a8.5 8.5 0 1 0 8.045 9.909c.22.366.542.633 1.078.633q.185 0 .338-.04C20.868 18.57 16.835 22 12 22C6.477 22 2 17.523 2 12S6.477 2 12 2m-1.144 6.155A1.25 1.25 0 0 0 9 9.248v5.504a1.25 1.25 0 0 0 1.856 1.093l5.757-3.189a.75.75 0 0 0 0-1.312z"/>
-                </svg>
-                <span>#{title_online}</span>
-            </div>
-        `;
-
-        Lampa.Listener.follow('full', function(e) {
-            if (e.type == 'complite') {
-                var btn = $(Lampa.Lang.translate(button));
-                btn.on('hover:enter', function() {
-                    Lampa.Component.add('eneyida', component);
-                    Lampa.Activity.push({
-                        url: '',
-                        title: Lampa.Lang.translate('title_online'),
-                        component: 'eneyida',
-                        search: e.data.movie.title,
-                        movie: e.data.movie,
-                        page: 1
-                    });
+                Lampa.Listener.follow('full', function(e) {
+                    if (e.type == 'complite') {
+                        var btn = $(Lampa.Lang.translate(button));
+                        btn.on('hover:enter', function() {
+                            Lampa.Component.add('eneyida', component);
+                            Lampa.Activity.push({
+                                url: '',
+                                title: Lampa.Lang.translate('title_online'),
+                                component: 'eneyida',
+                                search: e.data.movie.title,
+                                movie: e.data.movie,
+                                page: 1
+                            });
+                        });
+                        e.render.before(btn);
+                    }
                 });
-                e.render.before(btn);
-            }
-        });
 
-        Lampa.Component.add('eneyida', component);
-        Lampa.Plugin.register('eneyida', plugin);
+                Lampa.Component.add('eneyida', component);
+                Lampa.Plugin.register('eneyida', plugin);
+            } else if (attempts > 0) {
+                setTimeout(function() {
+                    tryRegister(attempts - 1);
+                }, 100);
+            } else {
+                console.error('Lampa.Plugin is undefined after maximum attempts');
+            }
+        }
+
+        // Спробувати зареєструвати плагін із максимум 50 спробами
+        tryRegister(50);
     }
 
     if (!window.eneyida_plugin) startPlugin();
