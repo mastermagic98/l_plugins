01a0e7ea66efdf16ece90db700d3d52312f1196d
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-07-04 16:17:43 +0300

    Update eneyida_plugin.js

--- eneyida_plugin.js
+++ eneyida_plugin.js
@@ -43,7 +43,7 @@
         // Додавання параметрів до URL
         function account(url) {
             url = url + '';
-            if (url.indexOf('uid=') == -1) {
+            if (url.indexOf('uid=') === -1) {
                 url = Lampa.Utils.addUrlComponent(url, 'uid=' + encodeURIComponent(unic_id));
             }
             return url;
@@ -76,7 +76,7 @@
             };
             filter.onBack = function() { _this.start(); };
             filter.onSelect = function(type, a, b) {
-                if (type == 'filter') {
+                if (type === 'filter') {
                     if (a.reset) {
                         var id = Lampa.Utils.hash(object.movie.number_of_seasons ? object.movie.original_name : object.movie.original_title);
                         var all = Lampa.Storage.get('clarification_search', '{}');
@@ -90,7 +90,7 @@
                     } else {
                         var url = filter_find[a.stype][b.index].url;
                         var choice = _this.getChoice();
-                        if (a.stype == 'voice') {
+                        if (a.stype === 'voice') {
                             choice.voice_name = filter_find.voice[b.index].title;
                             choice.voice_url = url;
                         }
@@ -100,7 +100,7 @@
                         _this.request(url);
                         setTimeout(Lampa.Select.close, 10);
                     }
-                } else if (type == 'sort') {
+                } else if (type === 'sort') {
                     Lampa.Select.close();
                     _this.changeBalanser(a.source);
                 }
@@ -169,7 +169,7 @@
                     if (voices.length) {
                         filter_find.voice = voices;
                         var choice = this.getChoice();
-                        var voice_index = voices.findIndex(function(v) { return v.url == choice.voice_url || v.title == choice.voice_name; });
+                        var voice_index = voices.findIndex(function(v) { return v.url === choice.voice_url || v.title === choice.voice_name; });
                         if (voice_index >= 0 && !voices[voice_index].active) {
                             this.replaceChoice({ voice: voice_index, voice_name: voices[voice_index].title });
                             this.request(voices[voice_index].url);
@@ -307,8 +307,9 @@
 
         // Завантаження
         this.loading = function(status) {
-            if (status) this.activity.loader(true);
-            else {
+            if (status) {
+                this.activity.loader(true);
+            } else {
                 this.activity.loader(false);
                 this.activity.toggle();
             }
@@ -322,17 +323,21 @@
                 var subitems = [];
                 var value = choice[type] || 0;
                 items.forEach(function(name, i) {
-                    subitems.push({ title: name, selected: value == i, index: i });
+                    subitems.push({ title: name, selected: value === i, index: i });
                 });
                 select.push({ title: title, subtitle: items[value], items: subitems, stype: type });
             };
             filter_items.source = filter_sources;
             select.push({ title: Lampa.Lang.translate('video_parser_reset'), reset: true });
-            if (filter_items.voice && filter_items.voice.length) add('voice', Lampa.Lang.translate('video_parser_voice'));
-            if (filter_items.season && filter_items.season.length) add('season', Lampa.Lang.translate('video_serial_season'));
+            if (filter_items.voice && filter_items.voice.length) {
+                add('voice', Lampa.Lang.translate('video_parser_voice'));
+            }
+            if (filter_items.season && filter_items.season.length) {
+                add('season', Lampa.Lang.translate('video_serial_season'));
+            }
             filter.set('filter', select);
             filter.set('sort', filter_sources.map(function(e) {
-                return { title: sources[e].name, source: e, selected: e == balanser });
+                return { title: sources[e].name, source: e, selected: e === balanser };
             }));
         };
 
@@ -346,7 +351,9 @@
 
         // Початок навігації
         this.start = function() {
-            if (Lampa.Activity.active().activity !== this.activity) return;
+            if (Lampa.Activity.active().activity !== this.activity) {
+                return;
+            }
             this.initialize();
             Lampa.Background.immediately(Lampa.Utils.cardImgBackgroundBlur(object.movie));
             Lampa.Controller.add('content', {
@@ -378,7 +385,7 @@
         window.lampac_plugin = true;
         var manifst = {
             type: 'video',
-            version: '1.4.15', // Оновлено версію
+            version: '1.4.16', // Оновлено версію
             name: 'Eneyida',
             description: 'Плагін для пошуку та перегляду фільмів і серіалів',
             component: 'lampac'
@@ -400,79 +407,83 @@
         });
 
         // CSS шаблон
-        Lampa.Template.add('lampac_css', `
-            <style>
-            .online-prestige{position:relative;border-radius:.3em;background-color:rgba(0,0,0,0.3);display:flex}
-            .online-prestige__body{padding:1.2em;line-height:1.3;flex-grow:1}
-            @media screen and (max-width:480px){.online-prestige__body{padding:.8em 1.2em}}
-            .online-prestige__img{position:relative;width:13em;flex-shrink:0;min-height:8.2em}
-            .online-prestige__img>img{position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;border-radius:.3em;opacity:0;transition:opacity .3s}
-            .online-prestige__img--loaded>img{opacity:1}
-            @media screen and (max-width:480px){.online-prestige__img{width:7em;min-height:6em}}
-            .online-prestige__loader{position:absolute;top:50%;left:50%;width:2em;height:2em;margin-left:-1em;margin-top:-1em;background:url(./img/loader.svg) no-repeat center center;background-size:contain}
-            .online-prestige__head,.online-prestige__footer{display:flex;justify-content:space-between;align-items:center}
-            .online-prestige__title{font-size:1.7em;overflow:hidden;text-overflow:ellipsis;display:-webkit-box;-webkit-line-clamp:1;-webkit-box-orient:vertical}
-            @media screen and (max-width:480px){.online-prestige__title{font-size:1.4em}}
-            .online-prestige__time{padding-left:2em}
-            .online-prestige__info{display:flex;align-items:center}
-            .online-prestige__quality{padding-left:1em;white-space:nowrap}
-            .online-prestige.focus::after{content:'';position:absolute;top:-0.6em;left:-0.6em;right:-0.6em;bottom:-0.6em;border-radius:.7em;border:solid .3em #fff;z-index:-1}
-            .online-prestige+.online-prestige{margin-top:1.5em}
-            .online-empty{line-height:1.4}
-            .online-empty__title{font-size:1.8em;margin-bottom:.3em}
-            .online-empty__time{font-size:1.2em;font-weight:300;margin-bottom:1.6em}
-            .online-empty__buttons{display:flex}
-            .online-empty__buttons>*+*{margin-left:1em}
-            .online-empty__button{background:rgba(0,0,0,0.3);font-size:1.2em;padding:.5em 1.2em;border-radius:.2em;margin-bottom:2.4em}
-            .online-empty__button.focus{background:#fff;color:black}
-            </style>
-        `);
+        Lampa.Template.add('lampac_css', [
+            '<style>',
+            '.online-prestige{position:relative;border-radius:.3em;background-color:rgba(0,0,0,0.3);display:flex}',
+            '.online-prestige__body{padding:1.2em;line-height:1.3;flex-grow:1}',
+            '@media screen and (max-width:480px){.online-prestige__body{padding:.8em 1.2em}}',
+            '.online-prestige__img{position:relative;width:13em;flex-shrink:0;min-height:8.2em}',
+            '.online-prestige__img>img{position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;border-radius:.3em;opacity:0;transition:opacity .3s}',
+            '.online-prestige__img--loaded>img{opacity:1}',
+            '@media screen and (max-width:480px){.online-prestige__img{width:7em;min-height:6em}}',
+            '.online-prestige__loader{position:absolute;top:50%;left:50%;width:2em;height:2em;margin-left:-1em;margin-top:-1em;background:url(./img/loader.svg) no-repeat center center;background-size:contain}',
+            '.online-prestige__head,.online-prestige__footer{display:flex;justify-content:space-between;align-items:center}',
+            '.online-prestige__title{font-size:1.7em;overflow:hidden;text-overflow:ellipsis;display:-webkit-box;-webkit-line-clamp:1;-webkit-box-orient:vertical}',
+            '@media screen and (max-width:480px){.online-prestige__title{font-size:1.4em}}',
+            '.online-prestige__time{padding-left:2em}',
+            '.online-prestige__info{display:flex;align-items:center}',
+            '.online-prestige__quality{padding-left:1em;white-space:nowrap}',
+            '.online-prestige.focus::after{content:\'\';position:absolute;top:-0.6em;left:-0.6em;right:-0.6em;bottom:-0.6em;border-radius:.7em;border:solid .3em #fff;z-index:-1}',
+            '.online-prestige+.online-prestige{margin-top:1.5em}',
+            '.online-empty{line-height:1.4}',
+            '.online-empty__title{font-size:1.8em;margin-bottom:.3em}',
+            '.online-empty__time{font-size:1.2em;font-weight:300;margin-bottom:1.6em}',
+            '.online-empty__buttons{display:flex}',
+            '.online-empty__buttons>*+*{margin-left:1em}',
+            '.online-empty__button{background:rgba(0,0,0,0.3);font-size:1.2em;padding:.5em 1.2em;border-radius:.2em;margin-bottom:2.4em}',
+            '.online-empty__button.focus{background:#fff;color:black}',
+            '</style>'
+        ].join(''));
         $('body').append(Lampa.Template.get('lampac_css', {}, true));
 
         // Шаблони
         function resetTemplates() {
-            Lampa.Template.add('lampac_prestige_full', `
-                <div class="online-prestige selector">
-                    <div class="online-prestige__img">
-                        <img alt="">
-                        <div class="online-prestige__loader"></div>
-                    </div>
-                    <div class="online-prestige__body">
-                        <div class="online-prestige__head">
-                            <div class="online-prestige__title">{title}</div>
-                            <div class="online-prestige__time">{time}</div>
-                        </div>
-                        <div class="online-prestige__footer">
-                            <div class="online-prestige__info">{info}</div>
-                            <div class="online-prestige__quality">{quality}</div>
-                        </div>
-                    </div>
-                </div>`);
-            Lampa.Template.add('lampac_content_loading', `
-                <div class="online-empty">
-                    <div class="broadcast__scan"><div></div></div>
-                </div>`);
-            Lampa.Template.add('lampac_does_not_answer', `
-                <div class="online-empty">
-                    <div class="online-empty__title">#{lampac_balanser_dont_work}</div>
-                    <div class="online-empty__time">#{lampac_does_not_answer_text}</div>
-                    <div class="online-empty__buttons">
-                        <div class="online-empty__button selector cancel">#{cancel}</div>
-                        <div class="online-empty__button selector change">#{lampac_change_balanser}</div>
-                    </div>
-                </div>`);
+            Lampa.Template.add('lampac_prestige_full', [
+                '<div class="online-prestige selector">',
+                '<div class="online-prestige__img">',
+                '<img alt="">',
+                '<div class="online-prestige__loader"></div>',
+                '</div>',
+                '<div class="online-prestige__body">',
+                '<div class="online-prestige__head">',
+                '<div class="online-prestige__title">{title}</div>',
+                '<div class="online-prestige__time">{time}</div>',
+                '</div>',
+                '<div class="online-prestige__footer">',
+                '<div class="online-prestige__info">{info}</div>',
+                '<div class="online-prestige__quality">{quality}</div>',
+                '</div>',
+                '</div>',
+                '</div>'
+            ].join(''));
+            Lampa.Template.add('lampac_content_loading', [
+                '<div class="online-empty">',
+                '<div class="broadcast__scan"><div></div></div>',
+                '</div>'
+            ].join(''));
+            Lampa.Template.add('lampac_does_not_answer', [
+                '<div class="online-empty">',
+                '<div class="online-empty__title">#{lampac_balanser_dont_work}</div>',
+                '<div class="online-empty__time">#{lampac_does_not_answer_text}</div>',
+                '<div class="online-empty__buttons">',
+                '<div class="online-empty__button selector cancel">#{cancel}</div>',
+                '<div class="online-empty__button selector change">#{lampac_change_balanser}</div>',
+                '</div>',
+                '</div>'
+            ].join(''));
         }
 
-        var button = `
-            <div class="full-start__button selector view--online lampac--button" data-subtitle="${manifst.name} v${manifst.version}">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 392.697 392.697">
-                    <path d="M21.837,83.419l36.496,16.678L227.72,19.886c1.229-0.592,2.002-1.846,1.98-3.209c-0.021-1.365-0.834-2.592-2.082-3.145L197.766,0.3c-0.903-0.4-1.933-0.4-2.837,0L21.873,77.036c-1.259,0.559-2.073,1.803-2.081,3.18C19.784,81.593,20.584,82.847,21.837,83.419z" fill="currentColor"></path>
-                    <path d="M185.689,177.261l-64.988-30.01v91.617c0,0.856-0.44,1.655-1.167,2.114c-0.406,0.257-0.869,0.386-1.333,0.386c-0.368,0-0.736-0.082-1.079-0.244l-68.874-32.625c-0.869-0.416-1.421-1.293-1.421-2.256v-92.229L6.804,95.5c-1.083-0.496-2.344-0.406-3.347,0.238c-1.002,0.645-1.608,1.754-1.608,2.944v208.744c0,1.371,0.799,2.615,2.045,3.185l178.886,81.768c0.464,0.211,0.96,0.315,1.455,0.315c0.661,0,1.318-0.188,1.892-0.555c1.002-0.645,1.608-1.754,1.608-2.945V180.445C187.735,179.076,186.936,177.831,185.689,177.261z" fill="currentColor"></path>
-                    <path d="M389.24,95.74c-1.002-0.644-2.264-0.732-3.347-0.238l-178.876,81.76c-1.246,0.57-2.045,1.814-2.045,3.185v208.751c0,1.191,0.606,2.302,1.608,2.945c0.572,0.367,1.23,0.555,1.892,0.555c0.495,0,0.991-0.104,1.455-0.315l178.876-81.768c1.246-0.568,2.045-1.813,2.045-3.185V98.685C390.849,97.494,390.242,96.384,389.24,95.74z" fill="currentColor"></path>
-                    <path d="M372.915,80.216c-0.009-1.377-0.823-2.621-2.082-3.18l-60.182-26.681c-0.938-0.418-2.013-0.399-2.938,0.045l-173.755,82.992l60.933,29.117c0.462,0.211,0.958,0.316,1.455,0.316s0.993-0.105,1.455-0.316l173.066-79.092C372.122,82.847,372.923,81.593,372.915,80.216z" fill="currentColor"></path>
-                </svg>
-                <span>#{title_online}</span>
-            </div>`;
+        var button = [
+            '<div class="full-start__button selector view--online lampac--button" data-subtitle="' + manifst.name + ' v' + manifst.version + '">',
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 392.697 392.697">',
+            '<path d="M21.837,83.419l36.496,16.678L227.72,19.886c1.229-0.592,2.002-1.846,1.98-3.209c-0.021-1.365-0.834-2.592-2.082-3.145L197.766,0.3c-0.903-0.4-1.933-0.4-2.837,0L21.873,77.036c-1.259,0.559-2.073,1.803-2.081,3.18C19.784,81.593,20.584,82.847,21.837,83.419z" fill="currentColor"></path>',
+            '<path d="M185.689,177.261l-64.988-30.01v91.617c0,0.856-0.44,1.655-1.167,2.114c-0.406,0.257-0.869,0.386-1.333,0.386c-0.368,0-0.736-0.082-1.079-0.244l-68.874-32.625c-0.869-0.416-1.421-1.293-1.421-2.256v-92.229L6.804,95.5c-1.083-0.496-2.344-0.406-3.347,0.238c-1.002,0.645-1.608,1.754-1.608,2.944v208.744c0,1.371,0.799,2.615,2.045,3.185l178.886,81.768c0.464,0.211,0.96,0.315,1.455,0.315c0.661,0,1.318-0.188,1.892-0.555c1.002-0.645,1.608-1.754,1.608-2.945V180.445C187.735,179.076,186.936,177.831,185.689,177.261z" fill="currentColor"></path>',
+            '<path d="M389.24,95.74c-1.002-0.644-2.264-0.732-3.347-0.238l-178.876,81.76c-1.246,0.57-2.045,1.814-2.045,3.185v208.751c0,1.191,0.606,2.302,1.608,2.945c0.572,0.367,1.23,0.555,1.892,0.555c0.495,0,0.991-0.104,1.455-0.315l178.876-81.768c1.246-0.568,2.045-1.813,2.045-3.185V98.685C390.849,97.494,390.242,96.384,389.24,95.74z" fill="currentColor"></path>',
+            '<path d="M372.915,80.216c-0.009-1.377-0.823-2.621-2.082-3.18l-60.182-26.681c-0.938-0.418-2.013-0.399-2.938,0.045l-173.755,82.992l60.933,29.117c0.462,0.211,0.958,0.316,1.455,0.316s0.993-0.105,1.455-0.316l173.066-79.092C372.122,82.847,372.923,81.593,372.915,80.216z" fill="currentColor"></path>',
+            '</svg>',
+            '<span>#{title_online}</span>',
+            '</div>'
+        ].join('');
 
         // Реєстрація компонента один раз
         if (!componentRegistered) {
@@ -518,7 +529,7 @@
                     Lampa.Noty.show(Lampa.Lang.translate('title_error'));
                 }
             });
-            render.eq(0).append(btn); // Додаємо лише в перший елемент
+            render.eq(0).append(btn);
             console.log('Eneyida Plugin: Button added', {
                 selector: render[0] ? render[0].className : 'none',
                 buttonCount: render.find('.lampac--button').length
@@ -556,7 +567,9 @@
                     timeElapsed: Date.now() - startTime
                 });
                 clearInterval(initInterval);
-                if (!buttonAdded) logParentSelectors();
+                if (!buttonAdded) {
+                    logParentSelectors();
+                }
                 return;
             }
             try {
@@ -649,5 +662,7 @@
         setTimeout(setupListener, 100);
     }
 
-    if (!window.lampac_plugin) startPlugin();
+    if (!window.lampac_plugin) {
+        startPlugin();
+    }
 })();
