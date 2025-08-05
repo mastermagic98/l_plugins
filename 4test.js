f882bd8a2092a23ec1e8b420a8eaa18e97f572eb
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-08-05 10:07:35 +0300

    Update 4test.js

--- 4test.js
+++ 4test.js
@@ -1,15 +1,20 @@
 (function() {
     'use strict';
 
+    // Set platform to TV mode
     Lampa.Platform.tv();
-    
+
     (function() {
         'use strict';
-        
+
         function main() {
-            // Anti-debugging protection (removed in deobfuscation)
-            
-                       // Load saved theme from localStorage
+            // Check platform access
+            if (Lampa.Manifest.origin !== 'bylampa') {
+                Lampa.Noty.show('Ошибка доступа');
+                return;
+            }
+
+            // Load saved theme from localStorage
             var savedTheme = localStorage.getItem('selectedTheme');
             if (savedTheme) {
                 var themeLink = $('<link rel="stylesheet" href="' + savedTheme + '">');
@@ -18,23 +23,23 @@
 
             // Add settings parameter for themes
             Lampa.SettingsApi.addParam({
-                'component': 'interface',
-                'param': {
-                    'name': 'my_themes',
-                    'type': 'static'
+                component: 'interface',
+                param: {
+                    name: 'my_themes',
+                    type: 'static'
                 },
-                'field': {
-                    'name': 'Мои темы',
-                    'description': 'Измени палитру элементов приложения'
+                field: {
+                    name: 'Мои темы',
+                    description: 'Измени палитру элементов приложения'
                 },
-                'onRender': function(item) {
+                onRender: function(item) {
                     setTimeout(function() {
                         $('.settings-param > div:contains("Мои темы")').parent().insertAfter($('<div class="my_themes category-full"></div>'));
                         
                         item.on('hover:enter', function() {
                             setTimeout(function() {
                                 if ($('.settings-folder').length || $('#stantion_filtr').length) {
-                                    window.history.backward();
+                                    window.history.back();
                                 }
                             }, 50);
                             
@@ -46,10 +51,10 @@
                                     params = JSON.parse(JSON.stringify(currentThemes));
                                 } else {
                                     params = {
-                                        'url': 'https://bylampa.github.io/themes/categories/color_gallery.json',
-                                        'title': 'Focus Pack',
-                                        'component': 'my_themes',
-                                        'page': 1
+                                        url: 'https://bylampa.github.io/themes/categories/color_gallery.json',
+                                        title: 'Focus Pack',
+                                        component: 'my_themes',
+                                        page: 1
                                     };
                                 }
                                 
@@ -61,13 +66,13 @@
                 }
             });
 
-            // Theme component constructor
+            // Theme component constructor function
             function ThemeComponent(params) {
                 var network = new Lampa.Reguest();
                 var scroll = new Lampa.Scroll({
-                    'mask': true,
-                    'over': true,
-                    'step': 250
+                    mask: true,
+                    over: true,
+                    step: 250
                 });
                 var items = [];
                 var html = $('<div></div>');
@@ -75,9 +80,9 @@
                 var info, last, active;
                 
                 var categories = [
-                    {'title': 'Focus Pack', 'url': 'https://bylampa.github.io/themes/categories/color_gallery.json'},
-                    {'title': 'Color Gallery', 'url': 'https://bylampa.github.io/themes/categories/stroke.json'},
-                    {'title': 'Gradient Style', 'url': 'https://bylampa.github.io/themes/categories/gradient_style.json'}
+                    {title: 'Focus Pack', url: 'https://bylampa.github.io/themes/categories/color_gallery.json'},
+                    {title: 'Color Gallery', url: 'https://bylampa.github.io/themes/categories/stroke.json'},
+                    {title: 'Gradient Style', url: 'https://bylampa.github.io/themes/categories/gradient_style.json'}
                 ];
 
                 this.create = function() {
@@ -101,13 +106,13 @@
                     
                     data.forEach(function(element) {
                         var item = Lampa.Template.get('card', {
-                            'title': element.title,
-                            'release_year': ''
+                            title: element.title,
+                            release_year: ''
                         });
                         
                         item.addClass('card--collection');
                         item.find('.card__img').css({
-                            'cursor': 'pointer',
+                            cursor: 'pointer',
                             'background-color': '#353535a6'
                         });
                         item.css({'text-align': 'center'});
@@ -126,7 +131,7 @@
                         
                         $('.info__title').remove();
 
-                        // Add "Установлена" (Installed) label function
+                        // Function to add "Установлена" (Installed) label
                         function addInstalledLabel() {
                             var label = document.createElement('div');
                             label.innerText = 'Установлена';
@@ -134,17 +139,17 @@
                             item.find('.card__img').append(label);
                             
                             $(label).css({
-                                'position': 'absolute',
-                                'left': '-3%',
-                                'bottom': '70%',
-                                'padding': '0.4em 0.4em',
-                                'background': '#ffe216',
-                                'color': '#000',
-                                'fontSize': '0.8em',
-                                'WebkitBorderRadius': '0.3em',
-                                'MozBorderRadius': '0.3em',
-                                'borderRadius': '0.3em',
-                                'textTransform': 'uppercase'
+                                position: 'absolute',
+                                left: '-3%',
+                                bottom: '70%',
+                                padding: '0.4em 0.4em',
+                                background: '#ffe216',
+                                color: '#000',
+                                fontSize: '0.8em',
+                                WebkitBorderRadius: '0.3em',
+                                MozBorderRadius: '0.3em',
+                                borderRadius: '0.3em',
+                                textTransform: 'uppercase'
                             });
                         }
 
@@ -166,16 +171,16 @@
                             var controller = Lampa.Controller.enabled().name;
                             var actions = [];
                             
-                            actions.push({'title': 'Установить'});
-                            actions.push({'title': 'Удалить'});
+                            actions.push({title: 'Установить'});
+                            actions.push({title: 'Удалить'});
                             
                             Lampa.Select.show({
-                                'title': '',
-                                'items': actions,
-                                'onBack': function() {
+                                title: '',
+                                items: actions,
+                                onBack: function() {
                                     Lampa.Controller.toggle('content');
                                 },
-                                'onSelect': function(action) {
+                                onSelect: function(action) {
                                     if (action.title === 'Установить') {
                                         // Remove existing theme links
                                         $('link[rel="stylesheet"][href^="https://bylampa.github.io/themes/css/"]').remove();
@@ -254,8 +259,44 @@
                     var self = this;
                     
                     Lampa.Background.change('');
-                    Lampa.Template.add('button_category', '<div id=\'button_category\'><style>@media screen and (max-width: 2560px) {.themes .card--collection {width: 14.2%!important;}.scroll__content {padding:1.5em 0!important;}.info {height:9em!important;}.info__title-original {font-size:1.2em;}}@media screen and (max-width: 385px) {.info__right {display:contents!important;}.themes .card--collection {width: 33.3%!important;}}@media screen and (max-width: 580px) {.info__right {display:contents!important;}.themes .card--collection {width: 25%!important;}}</style><div class="full-start__button selector view--category"><svg style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 24 24" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="info"/><g id="icons"><g id="menu"><path d="M20,10H4c-1.1,0-2,0.9-2,2c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2C22,10.9,21.1,10,20,10z" fill="currentColor"/><path d="M4,8h12c1.1,0,2-0.9,2-2c0-1.1-0.9-2-2-2H4C2.9,4,2,4.9,2,6C2,7.1,2.9,8,4,8z" fill="currentColor"/><path d="M16,16H4c-1.1,0-2,0.9-2,2c0,1.1,0.9,2,2,2h12c1.1,0,2-0.9,2-2C18,16.9,17.1,16,16,16z" fill="currentColor"/></g></g></svg> <span>Категории тем</span>\n </div></div>');
-                    Lampa.Template.add('info_tvtv', '<div class="info layer--width"><div class="info__left"><div class="info__title"></div><div class="info__title-original"></div><div class="info__create"></div></div><div class="info__right">  <div id="stantion_filtr"></div></div></div>');
+                    
+                    // Add button category template with responsive CSS
+                    Lampa.Template.add('button_category', 
+                        '<div id="button_category">' +
+                        '<style>' +
+                        '@media screen and (max-width: 2560px) {.themes .card--collection {width: 14.2%!important;}.scroll__content {padding:1.5em 0!important;}.info {height:9em!important;}.info__title-original {font-size:1.2em;}}' +
+                        '@media screen and (max-width: 385px) {.info__right {display:contents!important;}.themes .card--collection {width: 33.3%!important;}}' +
+                        '@media screen and (max-width: 580px) {.info__right {display:contents!important;}.themes .card--collection {width: 25%!important;}}' +
+                        '</style>' +
+                        '<div class="full-start__button selector view--category">' +
+                        '<svg style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 24 24" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">' +
+                        '<g id="info"/>' +
+                        '<g id="icons">' +
+                        '<g id="menu">' +
+                        '<path d="M20,10H4c-1.1,0-2,0.9-2,2c0,1.1,0.9,2,2,2h16c1.1,0,2-0.9,2-2C22,10.9,21.1,10,20,10z" fill="currentColor"/>' +
+                        '<path d="M4,8h12c1.1,0,2-0.9,2-2c0-1.1-0.9-2-2-2H4C2.9,4,2,4.9,2,6C2,7.1,2.9,8,4,8z" fill="currentColor"/>' +
+                        '<path d="M16,16H4c-1.1,0-2,0.9-2,2c0,1.1,0.9,2,2,2h12c1.1,0,2-0.9,2-2C18,16.9,17.1,16,16,16z" fill="currentColor"/>' +
+                        '</g>' +
+                        '</g>' +
+                        '</svg>' +
+                        ' <span>Категории тем</span>' +
+                        '</div>' +
+                        '</div>'
+                    );
+                    
+                    // Add info template
+                    Lampa.Template.add('info_tvtv', 
+                        '<div class="info layer--width">' +
+                        '<div class="info__left">' +
+                        '<div class="info__title"></div>' +
+                        '<div class="info__title-original"></div>' +
+                        '<div class="info__create"></div>' +
+                        '</div>' +
+                        '<div class="info__right">' +
+                        '<div id="stantion_filtr"></div>' +
+                        '</div>' +
+                        '</div>'
+                    );
                     
                     var categoryButton = Lampa.Template.get('button_category');
                     info = Lampa.Template.get('info_tvtv');
@@ -281,18 +322,18 @@
 
                 this.selectGroup = function() {
                     Lampa.Select.show({
-                        'title': 'Категории тем',
-                        'items': categories,
-                        'onSelect': function(item) {
+                        title: 'Категории тем',
+                        items: categories,
+                        onSelect: function(item) {
                             Lampa.Activity.push({
-                                'url': item.url,
-                                'title': item.title,
-                                'component': 'my_themes',
-                                'page': 1
+                                url: item.url,
+                                title: item.title,
+                                component: 'my_themes',
+                                page: 1
                             });
                             Lampa.Storage.set('themesCurrent', JSON.stringify(Lampa.Activity.active()));
                         },
-                        'onBack': function() {
+                        onBack: function() {
                             Lampa.Controller.toggle('content');
                         }
                     });
@@ -302,25 +343,25 @@
                     var self = this;
                     
                     Lampa.Controller.add('content', {
-                        'toggle': function() {
+                        toggle: function() {
                             Lampa.Controller.collectionSet(scroll.render());
                             Lampa.Controller.collectionFocus(last || false, scroll.render());
                         },
-                        'left': function() {
+                        left: function() {
                             if (Navigator.canmove('left')) {
                                 Navigator.move('left');
                             } else {
                                 Lampa.Controller.toggle('menu');
                             }
                         },
-                        'right': function() {
+                        right: function() {
                             if (Navigator.canmove('right')) {
                                 Navigator.move('right');
                             } else {
                                 self.selectGroup();
                             }
                         },
-                        'up': function() {
+                        up: function() {
                             if (Navigator.canmove('up')) {
                                 Navigator.move('up');
                             } else {
@@ -332,14 +373,14 @@
                                 }
                             }
                         },
-                        'down': function() {
+                        down: function() {
                             if (Navigator.canmove('down')) {
                                 Navigator.move('down');
                             } else if (info.find('.view--category').hasClass('focus')) {
                                 Lampa.Controller.toggle('content');
                             }
                         },
-                        'back': function() {
+                        back: function() {
                             Lampa.Activity.backward();
                         }
                     });
