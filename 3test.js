f522096795d11962bd49afd3aab535ac4e41128f
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-08-07 12:13:18 +0300

    Update 3test.js

--- 3test.js
+++ 3test.js
@@ -8,7 +8,7 @@
         Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/dark_style.js'], function () { });
 
 	//Подборки
-	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/surs.js'], function () { });
+	//Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/surs.js'], function () { });
 		
 	//Настройка шапки
 	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/head_filter.js'], function () { });
@@ -17,22 +17,22 @@
 	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/lable_serial.js'], function () { });
 
 	//Логотипы в карточках
-	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/logo_title.js'], function () { });
+	//Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/logo_title.js'], function () { });
 
 	//Cardify
-	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/cardify.js'], function () { });
+	//Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/cardify.js'], function () { });
 
 	//Каталог парсеров
-	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/pubtorr.js'], function () { });
+	//Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/pubtorr.js'], function () { });
 
 	//HDRezka
-	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/hdrezka.js'], function () { });
+	//Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/hdrezka.js'], function () { });
 
 	//DMCA
-	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/dmca.js'], function () { });
+	//Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/dmca.js'], function () { });
 
 	//Trick
-	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/tricks.js'], function () { });
+	//Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/tricks.js'], function () { });
 
 
 })();
