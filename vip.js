54f359874e6d3bb0ef5ff7618829b5e97f8619b4
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-08-11 16:19:21 +0300

    Update vip.js

--- vip.js
+++ vip.js
@@ -1,23 +1,40 @@
 (function () {
     'use strict';
 
-    var plugin = function () {};
+    function init() {
+        var plugin = function () {};
 
-    plugin.prototype.boot = function () {
-        // Додаємо правила заміни
-        Lampa.Utils.putScriptReplace({
-            "checkPremium\\(\\) \\{": "checkPremium() { return 1;", // завжди преміум
-            "this.add = function \\(\\) \\{": "this.add = function () { return;", // блокуємо add()
-            "full-start__button selector button--subscribe": "full-start__button selector button--subscribe hide", // ховаємо кнопку
-            "\\[[^\n\r]+'/plugin/vast'": "['{localhost}/vast.js']" // заміна шляху vast.js
+        plugin.prototype.boot = function () {
+            // Виконуємо заміни у коді програми
+            Lampa.Utils.putScriptReplace({
+                "checkPremium\\(\\) \\{": "checkPremium() { return 1;",
+                "this.add = function \\(\\) \\{": "this.add = function () { return;",
+                "full-start__button selector button--subscribe": "full-start__button selector button--subscribe hide",
+                "\\[[^\n\r]+'/plugin/vast'": "['{localhost}/vast.js']"
+            });
+        };
+
+        plugin.prototype.start = function () {
+            console.log('[PremiumPatch] Плагін активований');
+        };
+
+        Lampa.Plugins.add({
+            id: 'premium_patch',
+            title: 'Premium Patch',
+            description: 'Активує преміум та приховує рекламу',
+            version: '1.0',
+            author: 'Ти',
+            onStart: function () {
+                var p = new plugin();
+                p.boot();
+                p.start();
+            }
         });
-    };
+    }
 
-    plugin.prototype.start = function () {
-        console.log('[PremiumPatch] Плагін активований');
-    };
-
-    Lampa.Plugin.register('premium_patch', plugin);
-
+    if (window.Lampa && Lampa.Plugins) {
+        init();
+    } else {
+        document.addEventListener('lampa-ready', init);
+    }
 })();
-
