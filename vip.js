07f908fd7152cb43af2cab649cb41c7eb316913a
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-08-11 16:28:13 +0300

    Update vip.js

--- vip.js
+++ vip.js
@@ -1,40 +1,31 @@
 (function () {
     'use strict';
 
-    function init() {
-        var plugin = function () {};
+    // Твої заміни
+    const replacements = {
+        "checkPremium\\(\\) \\{": "checkPremium() { return 1;",
+        "this.add = function \\(\\) \\{": "this.add = function () { return;",
+        "full-start__button selector button--subscribe": "full-start__button selector button--subscribe hide",
+        "\\[[^\n\r]+'/plugin/vast'": "['{localhost}/vast.js']"
+    };
 
-        plugin.prototype.boot = function () {
-            // Виконуємо заміни у коді програми
-            Lampa.Utils.putScriptReplace({
-                "checkPremium\\(\\) \\{": "checkPremium() { return 1;",
-                "this.add = function \\(\\) \\{": "this.add = function () { return;",
-                "full-start__button selector button--subscribe": "full-start__button selector button--subscribe hide",
-                "\\[[^\n\r]+'/plugin/vast'": "['{localhost}/vast.js']"
-            });
-        };
-
-        plugin.prototype.start = function () {
-            console.log('[PremiumPatch] Плагін активований');
-        };
-
-        Lampa.Plugins.add({
-            id: 'premium_patch',
-            title: 'Premium Patch',
-            description: 'Активує преміум та приховує рекламу',
-            version: '1.0',
-            author: 'Ти',
-            onStart: function () {
-                var p = new plugin();
-                p.boot();
-                p.start();
-            }
-        });
-    }
-
-    if (window.Lampa && Lampa.Plugins) {
-        init();
-    } else {
-        document.addEventListener('lampa-ready', init);
-    }
+    // Підміна функції appendChild, щоб перехопити завантаження Lampa.js
+    const originalAppendChild = Element.prototype.appendChild;
+    Element.prototype.appendChild = function (element) {
+        if (element.tagName === 'SCRIPT' && element.src.includes('/lampa.js')) {
+            fetch(element.src)
+                .then(r => r.text())
+                .then(code => {
+                    for (let pattern in replacements) {
+                        code = code.replace(new RegExp(pattern, 'g'), replacements[pattern]);
+                    }
+                    const blob = new Blob([code], { type: 'application/javascript' });
+                    const url = URL.createObjectURL(blob);
+                    element.src = url;
+                    originalAppendChild.call(this, element);
+                });
+            return element;
+        }
+        return originalAppendChild.call(this, element);
+    };
 })();
