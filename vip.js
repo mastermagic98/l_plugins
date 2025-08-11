f801ccd2eb538a8e499a131d5d0915774b3fba71
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-08-11 16:14:40 +0300

    Update vip.js

--- vip.js
+++ vip.js
@@ -1,1 +1,23 @@
+(function () {
+    'use strict';
+
+    var plugin = function () {};
+
+    plugin.prototype.boot = function () {
+        // Додаємо правила заміни
+        Lampa.Utils.putScriptReplace({
+            "checkPremium\\(\\) \\{": "checkPremium() { return 1;", // завжди преміум
+            "this.add = function \\(\\) \\{": "this.add = function () { return;", // блокуємо add()
+            "full-start__button selector button--subscribe": "full-start__button selector button--subscribe hide", // ховаємо кнопку
+            "\\[[^\n\r]+'/plugin/vast'": "['{localhost}/vast.js']" // заміна шляху vast.js
+        });
+    };
+
+    plugin.prototype.start = function () {
+        console.log('[PremiumPatch] Плагін активований');
+    };
+
+    Lampa.Plugin.register('premium_patch', plugin);
+
+})();
 
