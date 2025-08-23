b4f1ec28d6cd102af17d22b0b556ca01bc5ea89e
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-08-23 13:56:10 +0300

    Update th3.js

--- th3.js
+++ th3.js
@@ -108,13 +108,26 @@
             ':root {' +
                 '--main-color: ' + ColorPlugin.settings.main_color + ';' +
             '}' +
-            // Іконки заголовка і налаштувань праворуч - завжди білі
+            // Іконки меню ліворуч, заголовка і налаштувань праворуч - завжди білі
+            '.menu__ico, .menu__ico.focus, .menu__ico:hover, .menu__ico.traverse, ' +
             '.head__action, .head__action.focus, .head__action:hover, ' +
             '.settings-param__ico, .settings-param__ico.focus, .settings-param__ico:hover, ' +
             '.settings-param__icon, .settings-param__icon.focus, .settings-param__icon:hover, ' +
             '.settings__icon, .settings__icon.focus, .settings__icon:hover {' +
                 'color: #fff !important;' +
                 'fill: #fff !important;' +
+                'stroke: #fff !important;' +
+            '}' +
+            // Перекриваємо стилі з app.css для SVG-елементів у menu__ico
+            '.menu__item.focus .menu__ico path[fill], .menu__item.focus .menu__ico rect[fill], ' +
+            '.menu__item.focus .menu__ico circle[fill], .menu__item.traverse .menu__ico path[fill], ' +
+            '.menu__item.traverse .menu__ico rect[fill], .menu__item.traverse .menu__ico circle[fill], ' +
+            '.menu__item:hover .menu__ico path[fill], .menu__item:hover .menu__ico rect[fill], ' +
+            '.menu__item:hover .menu__ico circle[fill], .menu__ico [fill] {' +
+                'fill: #fff !important;' +
+            '}' +
+            '.menu__item.focus .menu__ico [stroke], .menu__item.traverse .menu__ico [stroke], ' +
+            '.menu__item:hover .menu__ico [stroke], .menu__ico [stroke] {' +
                 'stroke: #fff !important;' +
             '}' +
             // Перекриваємо стилі для іконок у меню налаштувань
