56c0b0e141a82c9e02b584e363e35c07b9bf1e98
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-08-25 12:00:20 +0300

    Update th3.js

--- th3.js
+++ th3.js
@@ -44,22 +44,145 @@
         colors: {
             main: {
                 'default': Lampa.Lang.translate('default_color'),
-                '#e71500': 'Темно-бордовий',
-                '#e1284e': 'Амарант',
-                '#0c705b': 'Вечірнє море',
-                '#2e8b2e': 'Мантіс',
-                '#3b88d5': 'Маринер',
-                '#1e779c': 'Блюмін',
-                '#008d8b': 'Бірюзовий',
-                '#823398': 'Фуксія',
-                '#944729': 'Охра'
+                '#fb2c36': 'Red 1',
+                '#e7000b': 'Red 2',
+                '#c10007': 'Red 3',
+                '#9f0712': 'Red 4',
+                '#82181a': 'Red 5',
+                '#460809': 'Red 6',
+                '#ff6900': 'Orange 1',
+                '#f54900': 'Orange 2',
+                '#ca3500': 'Orange 3',
+                '#9f2d00': 'Orange 4',
+                '#7e2a0c': 'Orange 5',
+                '#441306': 'Orange 6',
+                '#fe9a00': 'Amber 1',
+                '#e17100': 'Amber 2',
+                '#bb4d00': 'Amber 3',
+                '#973c00': 'Amber 4',
+                '#7b3306': 'Amber 5',
+                '#461901': 'Amber 6',
+                '#f0b100': 'Yellow 1',
+                '#d08700': 'Yellow 2',
+                '#a65f00': 'Yellow 3',
+                '#894b00': 'Yellow 4',
+                '#733e0a': 'Yellow 5',
+                '#432004': 'Yellow 6',
+                '#7ccf00': 'Lime 1',
+                '#5ea500': 'Lime 2',
+                '#497d00': 'Lime 3',
+                '#3c6300': 'Lime 4',
+                '#35530e': 'Lime 5',
+                '#192e03': 'Lime 6',
+                '#00c950': 'Green 1',
+                '#00a63e': 'Green 2',
+                '#008236': 'Green 3',
+                '#016630': 'Green 4',
+                '#0d542b': 'Green 5',
+                '#032e15': 'Green 6',
+                '#00bc7d': 'Emerald 1',
+                '#009966': 'Emerald 2',
+                '#007a55': 'Emerald 3',
+                '#006045': 'Emerald 4',
+                '#004f3b': 'Emerald 5',
+                '#002c22': 'Emerald 6',
+                '#00bba7': 'Teal 1',
+                '#009689': 'Teal 2',
+                '#00786f': 'Teal 3',
+                '#005f5a': 'Teal 4',
+                '#0b4f4a': 'Teal 5',
+                '#022f2e': 'Teal 6',
+                '#00b8db': 'Cyan 1',
+                '#0092b8': 'Cyan 2',
+                '#007595': 'Cyan 3',
+                '#005f78': 'Cyan 4',
+                '#104e64': 'Cyan 5',
+                '#053345': 'Cyan 6',
+                '#00a6f4': 'Sky 1',
+                '#0084d1': 'Sky 2',
+                '#0069a8': 'Sky 3',
+                '#00598a': 'Sky 4',
+                '#024a70': 'Sky 5',
+                '#052f4a': 'Sky 6',
+                '#2b7fff': 'Blue 1',
+                '#155dfc': 'Blue 2',
+                '#1447e6': 'Blue 3',
+                '#193cb8': 'Blue 4',
+                '#1c398e': 'Blue 5',
+                '#162456': 'Blue 6',
+                '#615fff': 'Indigo 1',
+                '#4f39f6': 'Indigo 2',
+                '#432dd7': 'Indigo 3',
+                '#372aac': 'Indigo 4',
+                '#312c85': 'Indigo 5',
+                '#1e1a4d': 'Indigo 6',
+                '#8e51ff': 'Violet 1',
+                '#7f22fe': 'Violet 2',
+                '#7008e7': 'Violet 3',
+                '#5d0ec0': 'Violet 4',
+                '#4d179a': 'Violet 5',
+                '#2f0d68': 'Violet 6',
+                '#ad46ff': 'Purple 1',
+                '#9810fa': 'Purple 2',
+                '#8200db': 'Purple 3',
+                '#6e11b0': 'Purple 4',
+                '#59168b': 'Purple 5',
+                '#3c0366': 'Purple 6',
+                '#e12afb': 'Fuchsia 1',
+                '#c800de': 'Fuchsia 2',
+                '#a800b7': 'Fuchsia 3',
+                '#8a0194': 'Fuchsia 4',
+                '#721378': 'Fuchsia 5',
+                '#4b004f': 'Fuchsia 6',
+                '#f6339a': 'Pink 1',
+                '#e60076': 'Pink 2',
+                '#c6005c': 'Pink 3',
+                '#a3004c': 'Pink 4',
+                '#861043': 'Pink 5',
+                '#510424': 'Pink 6',
+                '#ff2056': 'Rose 1',
+                '#ec003f': 'Rose 2',
+                '#c70036': 'Rose 3',
+                '#a50036': 'Rose 4',
+                '#8b0836': 'Rose 5',
+                '#4d0218': 'Rose 6',
+                '#62748e': 'Slate 1',
+                '#45556c': 'Slate 2',
+                '#314158': 'Slate 3',
+                '#1d293d': 'Slate 4',
+                '#0f172b': 'Slate 5',
+                '#020618': 'Slate 6',
+                '#6a7282': 'Gray 1',
+                '#4a5565': 'Gray 2',
+                '#364153': 'Gray 3',
+                '#1e2939': 'Gray 4',
+                '#101828': 'Gray 5',
+                '#030712': 'Gray 6',
+                '#71717b': 'Zinc 1',
+                '#52525c': 'Zinc 2',
+                '#3f3f46': 'Zinc 3',
+                '#27272a': 'Zinc 4',
+                '#18181b': 'Zinc 5',
+                '#09090b': 'Zinc 6',
+                '#737373': 'Neutral 1',
+                '#525252': 'Neutral 2',
+                '#404040': 'Neutral 3',
+                '#262626': 'Neutral 4',
+                '#171717': 'Neutral 5',
+                '#0a0a0a': 'Neutral 6',
+                '#79716b': 'Stone 1',
+                '#57534d': 'Stone 2',
+                '#44403b': 'Stone 3',
+                '#292524': 'Stone 4',
+                '#1c1917': 'Stone 5',
+                '#0c0a09': 'Stone 6'
             }
         }
     };
 
     // Функція для конвертації RGB у HEX
     function rgbToHex(rgb) {
-        var matches = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+        var matches = rgb.match(/^rgb$$ (\d+),\s*(\d+),\s*(\d+) $$$/);
         if (!matches) return rgb;
         function hex(n) {
             return ('0' + parseInt(n).toString(16)).slice(-2);
@@ -230,8 +353,8 @@
                 'content: "";' +
                 'position: absolute;' +
                 'top: 50%;' +
-                'left: 10%;' +
-                'right: 10%;' +
+                'left: 5%;' +
+                'right: 5%;' +
                 'height: 2px;' +
                 'background-color: #000;' +
                 'transform: rotate(45deg);' +
@@ -240,16 +363,16 @@
                 'content: "";' +
                 'position: absolute;' +
                 'top: 50%;' +
-                'left: 10%;' +
-                'right: 10%;' +
+                'left: 5%;' +
+                'right: 5%;' +
                 'height: 2px;' +
                 'background-color: #000;' +
                 'transform: rotate(-45deg);' +
             '}' +
             '.color_square {' +
-                'width: 45px;' +
-                'height: 45px;' +
-                'border-radius: 6px;' +
+                'width: 30px;' +
+                'height: 30px;' +
+                'border-radius: 4px;' +
             '}' +
             '.hex-input {' +
                 'width: 360px;' +
@@ -308,12 +431,12 @@
     // Функція для створення модального вікна вибору кольору
     function openColorPicker() {
         var colorKeys = Object.keys(ColorPlugin.colors.main);
-        var groupedColors = chunkArray(colorKeys, 5); // Сітка 5x2
+        var groupedColors = chunkArray(colorKeys, 5); // Сітка 5xN
         var colorContent = groupedColors.map(function (group) {
             var groupContent = group.map(function (color) {
                 return createColorHtml(color, ColorPlugin.colors.main[color]);
             }).join('');
-            return '<div style="display: grid; grid-template-columns: repeat(5, 1fr); gap: 11.25px; justify-items: center; padding: 10px;">' + groupContent + '</div>';
+            return '<div style="display: grid; grid-template-columns: repeat(5, 1fr); gap: 8px; justify-items: center; padding: 10px;">' + groupContent + '</div>';
         }).join('');
 
         // Блок для введення HEX-коду у стилі color_square
