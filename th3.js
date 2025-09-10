f08551b272c198886d084e77f6610580a6eb5456
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-09-10 18:17:17 +0300

    Update th3.js

--- th3.js
+++ th3.js
@@ -181,7 +181,7 @@
         }
         var component = Lampa.SettingsApi.components.find(function(c) { return c.component === 'color_plugin'; });
         if (component) {
-            component.icon = '<svg width="24px" height="24px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#ffffff"><path fill-rule="evenodd" clip-rule="evenodd" d="M8 1.003a7 7 0 0 0-7 7v.43c.09 1.51 1.91 1.79 3 .7a1.87 1.87 0 0 1 2.64 2.64c-1.1 1.16-.79 3.07.8 3.2h.6a7 7 0 1 0 0-14l-.04.03zm0 13h-.52a.58.58 0 0 1-.36-.14.56.56 0 0 1-.15-.3 1.24 1.24 0 0 1 .35-1.08 2.87 2.87 0 0 0 0-4 2.87 2.87 0 0 0-4.06 0 1 1 0 0 1-.9.34.41.41 0 0 1-.22-.12.42.42 0 0 1-.1-.29v-.37a6 6 0 1 1 6 6l-.04-.04zM9 3.997a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 7.007a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm-7-5a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm7-1a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM13 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/></svg>';
+            component.icon = '<svg width="24px" height="24px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#ffffff"><path fill-rule="evenodd" clip-rule="evenodd" d="M8 1.003a7 7 0 0 0-7 7v.43c.09 1.51 1.91 1.79 3 .7a1.87 1.87 0 0 1 2.64 2.64c-1.1 1.16-.79 3.07.8 3.2h.6a7 7 0 1 0 0-14l-.04.03zm0 13h-.52a.58.58 0 0 1-.36-.14.56.56 0 0 1-.15-.3 1.24 1.24 0 0 1 .35-1.08 2.87 2.87 0 0 0 0-4 2.87 2.87 0 0 0-4.06 0 1 1 0 0 1-.90.34.41.41 0 0 1-.22-.12.42.42 0 0 1-.1-.29v-.37a6 6 0 1 1 6 6l-.04-.04zM9 3.997a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 7.007a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm-7-5a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm7-1a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM13 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/></svg>';
             if (Lampa.Settings && Lampa.Settings.render) {
                 Lampa.Settings.render();
             }
@@ -729,40 +729,38 @@
             'color_plugin_highlight_enabled',
             'color_plugin_dimming_enabled'
         ];
-        setTimeout(function() {
-            var container = body || document;
-            for (var i = 0; i < params.length; i++) {
-                var selector = '.settings-param[data-name="' + params[i] + '"]';
-                var elements = container.querySelectorAll ? container.querySelectorAll(selector) : $(selector);
-                if (elements.length) {
-                    var displayValue = ColorPlugin.settings.enabled ? 'block' : 'none';
-                    for (var j = 0; j < elements.length; j++) {
-                        var element = elements[j];
-                        if (element.style) {
-                            element.style.display = displayValue;
-                        } else if (typeof $(element).css === 'function') {
-                            $(element).css('display', displayValue);
-                        }
+        var container = body || document;
+        for (var i = 0; i < params.length; i++) {
+            var selector = '.settings-param[data-name="' + params[i] + '"]';
+            var elements = container.querySelectorAll ? container.querySelectorAll(selector) : $(selector);
+            if (elements.length) {
+                var displayValue = ColorPlugin.settings.enabled ? 'block' : 'none';
+                for (var j = 0; j < elements.length; j++) {
+                    var element = elements[j];
+                    if (element.style) {
+                        element.style.display = displayValue;
+                    } else if (typeof $(element).css === 'function') {
+                        $(element).css('display', displayValue);
                     }
                 }
             }
-            // Альтернативне оновлення через Lampa.SettingsApi
-            if (Lampa.SettingsApi && Lampa.SettingsApi.params) {
-                var componentParams = Lampa.SettingsApi.params.filter(function(p) {
-                    return p.component === 'color_plugin';
-                });
-                for (var k = 0; k < componentParams.length; k++) {
-                    var param = componentParams[k];
-                    if (param.param.name !== 'color_plugin_enabled') {
-                        var paramElement = document.querySelector('.settings-param[data-name="' + param.param.name + '"]');
-                        if (paramElement && paramElement.style) {
-                            paramElement.style.display = ColorPlugin.settings.enabled ? 'block' : 'none';
-                        }
+        }
+        // Альтернативне оновлення через Lampa.SettingsApi
+        if (Lampa.SettingsApi && Lampa.SettingsApi.params) {
+            var componentParams = Lampa.SettingsApi.params.filter(function(p) {
+                return p.component === 'color_plugin';
+            });
+            for (var k = 0; k < componentParams.length; k++) {
+                var param = componentParams[k];
+                if (param.param.name !== 'color_plugin_enabled') {
+                    var paramElement = document.querySelector('.settings-param[data-name="' + param.param.name + '"]');
+                    if (paramElement && paramElement.style) {
+                        paramElement.style.display = ColorPlugin.settings.enabled ? 'block' : 'none';
                     }
                 }
             }
-            // Тимчасовий коментар для дебагінгу: Оновлено видимість параметрів
-        }, 1000);
+        }
+        // Тимчасовий коментар для дебагінгу: Оновлено видимість параметрів, елементи знайдено: + elements.length
     }
 
     // Функція для ініціалізації плагіна
