c0dd31d6e0f3d2dc25610ab0c1f1cd9a87d85634
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-12-11 00:43:35 +0200

    Update key.js

--- key.js
+++ key.js
@@ -10,17 +10,17 @@ function add() {
   if(elementXZ.length > 0) elementXZ.parent('div').hide();
 }
 
-/*setInterval(function() {
+setInterval(function() {
   var elementCHlang = $('div.hg-button.hg-functionBtn.hg-button-LANG.selector.binded')
   if (elementCHlang.length > 0){
-    Lampa.Storage.set('keyboard_default_lang', 'uk-UA')
+ //   Lampa.Storage.set('keyboard_default_lang', 'uk-UA')
     var elementUA = $('.selectbox-item.selector > div:contains("Русский")');
     if(elementUA.length > 0) elementUA.parent('div').hide();
-    Lampa.Storage.set('keyboard_default_lang', 'uk-UA')
+//    Lampa.Storage.set('keyboard_default_lang', 'uk-UA')
     var elementXZ = $('.selectbox-item.selector > div:contains("עִברִית")');
     if(elementXZ.length > 0) elementXZ.parent('div').hide();
   }
- }, 0)*/
+ }, 0)
 
 if(window.appready) add();
   else {
