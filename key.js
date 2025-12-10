e9cd4a7be84daf0edec44b7a5b204c48acbf2097
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-12-11 00:32:00 +0200

    Change default keyboard language to 'uk-UA'

--- key.js
+++ key.js
@@ -2,10 +2,10 @@
   'use strict';
 	
 function add() {
-  Lampa.Storage.set('keyboard_default_lang', 'default')
+  Lampa.Storage.set('keyboard_default_lang', 'uk-UA')
   var elementUA = $('.selectbox-item.selector > div:contains("Русский")');
   if(elementUA.length > 0) elementUA.parent('div').hide();
-  Lampa.Storage.set('keyboard_default_lang', 'default')
+  Lampa.Storage.set('keyboard_default_lang', 'uk-UA')
   var elementXZ = $('.selectbox-item.selector > div:contains("עִברִית")');
   if(elementXZ.length > 0) elementXZ.parent('div').hide();
 }
@@ -13,10 +13,10 @@ function add() {
 setInterval(function() {
   var elementCHlang = $('div.hg-button.hg-functionBtn.hg-button-LANG.selector.binded')
   if (elementCHlang.length > 0){
-    Lampa.Storage.set('keyboard_default_lang', 'default')
+    Lampa.Storage.set('keyboard_default_lang', 'uk-UA')
     var elementUA = $('.selectbox-item.selector > div:contains("Русский")');
     if(elementUA.length > 0) elementUA.parent('div').hide();
-    Lampa.Storage.set('keyboard_default_lang', 'default')
+    Lampa.Storage.set('keyboard_default_lang', 'uk-UA')
     var elementXZ = $('.selectbox-item.selector > div:contains("עִברִית")');
     if(elementXZ.length > 0) elementXZ.parent('div').hide();
   }
