e0e2f44eaea47c0ff647ca4f184084337341eafd
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-12-11 01:25:12 +0200

    Update key.js

--- key.js
+++ key.js
@@ -2,13 +2,13 @@
   'use strict';
 	
 function add() {
-/*  
+
   var elementUA = $('.selectbox-item.selector > div:contains("Русский")');
   if(elementUA.length > 0) elementUA.parent('div').hide();
-//  Lampa.Storage.set('keyboard_default_lang', 'uk-UA')
+  Lampa.Storage.set('keyboard_default_lang', 'ru')
   var elementXZ = $('.selectbox-item.selector > div:contains("עִברִית")');
   if(elementXZ.length > 0) elementXZ.parent('div').hide();
-} */
+
 
 setInterval(function() {
   var elementCHlang = $('div.hg-button.hg-functionBtn.hg-button-LANG.selector.binded')
