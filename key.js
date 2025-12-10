7b74bfdeef75ac03fde2dab3505ce43aab79885c
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-12-11 00:50:23 +0200

    Change default keyboard language to English

--- key.js
+++ key.js
@@ -2,7 +2,7 @@
   'use strict';
 	
 function add() {
-  Lampa.Storage.set('keyboard_default_lang', 'default')
+  Lampa.Storage.set('keyboard_default_lang', 'english')
   var elementUA = $('.selectbox-item.selector > div:contains("Русский")');
   if(elementUA.length > 0) elementUA.parent('div').hide();
 //  Lampa.Storage.set('keyboard_default_lang', 'uk-UA')
