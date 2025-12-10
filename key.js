700dee1050a6f1eb6f0b187316b87b1e7c808fd6
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-12-11 01:35:45 +0200

    Update key.js

--- key.js
+++ key.js
@@ -2,20 +2,21 @@
   'use strict';
 	
 function add() {
-
+  Lampa.Storage.set('keyboard_default_lang', 'default')
   var elementUA = $('.selectbox-item.selector > div:contains("Українська")');
   if(elementUA.length > 0) elementUA.parent('div').hide();
   Lampa.Storage.set('keyboard_default_lang', 'default')
   var elementXZ = $('.selectbox-item.selector > div:contains("עִברִית")');
   if(elementXZ.length > 0) elementXZ.parent('div').hide();
-
+}
 
 setInterval(function() {
   var elementCHlang = $('div.hg-button.hg-functionBtn.hg-button-LANG.selector.binded')
   if (elementCHlang.length > 0){
+    Lampa.Storage.set('keyboard_default_lang', 'default')
     var elementUA = $('.selectbox-item.selector > div:contains("Українська")');
     if(elementUA.length > 0) elementUA.parent('div').hide();
-//    Lampa.Storage.set('keyboard_default_lang', 'uk-UA')
+    Lampa.Storage.set('keyboard_default_lang', 'default')
     var elementXZ = $('.selectbox-item.selector > div:contains("עִברִית")');
     if(elementXZ.length > 0) elementXZ.parent('div').hide();
   }
@@ -30,4 +31,3 @@ if(window.appready) add();
     });
   }
 })();
-
