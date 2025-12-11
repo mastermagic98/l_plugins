933f833cb828dccde41f87262df71b3d2506b193
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-12-12 01:49:46 +0200

    Update test_board1.js

--- test_board1.js
+++ test_board1.js
@@ -31,7 +31,7 @@
         if (Lampa.Storage.get(k.he,'true') ==='true') $('.selectbox-item.selector > div:contains("עִברִית")').parent().hide();
     }
 
-    // Меню з нативними чекбоксами
+    /* Меню з нативними чекбоксами
    function openMenu = function() {
    var items = [];
 
